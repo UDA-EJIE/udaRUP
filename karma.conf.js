d41ff977fe13a6f1e9accb17fee92fd7256f67b2
sergisu <sergisu@gmail.com>
2017-03-22 16:34:54 +0100

    Modificación para TravisCI

--- karma.conf.js
+++ karma.conf.js
@@ -149,8 +149,8 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    //browsers: ['PhantomJS'],
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
+    //browsers: ['Chrome'],
   //  browserNoActivityTimeout:30000,
 
     // Continuous Integration mode
