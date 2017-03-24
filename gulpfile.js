8fbdd5212601dc9429020d8fb769df01dea6e44a
uGuinea <guinea.urko@gmail,com>
2017-03-24 09:26:14 +0100

    corregido error del script doc:api que evitaba generar los documentos

--- gulpfile.js
+++ gulpfile.js
@@ -363,25 +363,27 @@ gulp.task('doc:html', function (cb) {
 
 
 var runJsdoc2md = function (fileSource, outputPath) {
+    var basename;
     return gulp.src(fileSource)
         .pipe(tap(function (file, t) {
             basename = file.relative.split('.js')[0];
-            //console.log("File " + file.path + "  " + file.relative.split('.js')[0]);
+            console.log("outpath " + outputPath + " File " + file.path + "  " + basename);
             jsdoc2md.render({
                 files: file.path
-            }).then(output => fs.writeFile(outputPath + baseName + '.md', output));
+            }).then(output => fs.writeFile(outputPath + basename + '.md', output));
         }));
 };
 
 gulp.task('doc:api', function () {
 
-    var baseName;
+
     var fileSource = 'src/rup*.js';
     var outputPath = './doc/api/';
 
-    runJsdoc2md(fileSource, outputPath);
+    //runJsdoc2md(fileSource, outputPath);
     //rup_table
     fileSource = 'src/rup_table/rup*.js';
+    //outputPath = './doc/api/table/';
     runJsdoc2md(fileSource, outputPath);
 });
 
