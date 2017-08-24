0aefcedad59f9bf1e649309539428d09882fd8d0
uGuinea <guinea.urko@gmail,com>
2017-08-24 11:59:30 +0200

    added filtro y i18n al componente datatable

--- src/datatable/datatable.request.js
+++ src/datatable/datatable.request.js
@@ -1,23 +1,25 @@
 'use strict';
 
 
-class TableRequest{
+class TableRequest {
 
-	constructor(rows, page, sixd, sord){
+	constructor(rows, page, sixd, sord) {
 		this._rows = rows;
 		this._page = page;
 		this._sixd = sixd;
 		this._sord = sord;
+		this._filter;
+
 
 	}
 
-	getData(){
+	getData() {
 
 		return {
-			rows : this._rows,
-			page : this._page,
-			sixd : this._sixd,
-			sord : this._sord,
+			rows: this._rows,
+			page: this._page,
+			sixd: this._sixd,
+			sord: this._sord,
 			filter: this._filter,
 			nd: Date.now(),
 			core: {
@@ -30,37 +32,60 @@ class TableRequest{
 	}
 
 
-	set rows  (rows)  {this._rows = rows;}
-	get rows  ()      {return this._rows;}
-	set page  (page)  {this._page = page;}
-	get page  ()      {return this._page;}
-	set sixd  (sixd)  {this._sixd = sixd;}
-	get sixd  ()      {return this._sixd;}
-	set sord  (sord)  {this._sord = sord;}
-	get sord  ()      {return this._sord;}
+	set rows(rows) {
+		this._rows = rows;
+	}
+	get rows() {
+		return this._rows;
+	}
+	set page(page) {
+		this._page = page;
+	}
+	get page() {
+		return this._page;
+	}
+	set sixd(sixd) {
+		this._sixd = sixd;
+	}
+	get sixd() {
+		return this._sixd;
+	}
+	set sord(sord) {
+		this._sord = sord;
+	}
+	get sord() {
+		return this._sord;
+	}
 
-	set filter  (filter)  {this._filter = filter;}
-	get filter  ()      {return this._filter;}
+	set filter(filter) {
+		this._filter = filter;
+	}
+	get filter() {
+		return this._filter;
+	}
 
 
 
 }
 
 
-class DataTableRequest extends TableRequest{
+class DataTableRequest extends TableRequest {
 
-	constructor(data){
+	constructor(data) {
 		const start = data.start;
 		const length = data.length;
 
-		super(length, (start/length)+1, 'id', 'asc');
+		super(length, (start / length) + 1, 'id', 'asc');
 		this._data = data;
 		this._start = start;
 		this._length = length;
-
+		this._filter = data.filter;
 	}
 
 
 }
 
-export { TableRequest, DataTableRequest };
+export {
+	TableRequest,
+	DataTableRequest
+};
