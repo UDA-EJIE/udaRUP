c8ef4acbb0e3d06c533f03f909b0675af4376c68
enekoza <enekoza@gmail.com>
2018-01-16 12:40:10 +0100

    Modificado el Datatable a plugin de jQuery
    Se modifica el plugin de rup.datatables.
    
    En la primera implementación se había implementado mediante ES6.
    
    Se abandona el uso de ES6 para utlizar una implementación basada en plugins de jQuery con el loader de RUP.

--- src/datatable/datatable.request.js
+++ src/datatable/datatable.request.js
@@ -1,24 +1,85 @@
 'use strict';
 
-
-class TableRequest {
-
-	constructor(rows, page, sixd, sord) {
-		this._rows = rows;
-		this._page = page;
-		this._sixd = sixd;
-		this._sord = sord;
-		this._filter;
-
-
+(function (root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define([], factory);
+	} else if (typeof module === 'object' && module.exports) {
+		// Node. Does not work with strict CommonJS, but
+		// only CommonJS-like environments that support module.exports,
+		// like Node.
+		module.exports = factory();
+	} else {
+		// Browser globals (root is window)
+		root.returnExports = factory();
 	}
+}(typeof self !== 'undefined' ? self : this, function () {
 
-	getData() {
+	var DataTableRequest = function(data){
+
+		var start = data.start,
+			length = data.length,
+			page = (start / length) + 1,
+			sidx, sord;
+
+		if (data.order.length>0){
+			sidx = data.columns[data.order[0].column].data;
+			sord = data.order[0].dir;
+		}
+
+		// super(length, (start / length) + 1, 'id', 'asc');
+		this._data = data;
+		this._start = start;
+		this._length = length;
+		this._filter = data.filter;
+
+		this._rows = length;
+		this._page = page;
+		this._sidx = sidx;
+		this._sord = sord;
+		this._filter = data.filter;
+	};
+
+	DataTableRequest.prototype = {
+
+		set rows(rows) {
+			this._rows = rows;
+		},
+		get rows() {
+			return this._rows;
+		},
+		set page(page) {
+			this._page = page;
+		},
+		get page() {
+			return this._page;
+		},
+		set sidx(sidx) {
+			this._sidx = sidx;
+		},
+		get sidx() {
+			return this._sidx;
+		},
+		set sord(sord) {
+			this._sord = sord;
+		},
+		get sord() {
+			return this._sord;
+		},
+		set filter(filter) {
+			this._filter = filter;
+		},
+		get filter() {
+			return this._filter;
+		}
+	};
+
+	DataTableRequest.prototype.getData = function(){
 
 		return {
 			rows: this._rows,
 			page: this._page,
-			sixd: this._sixd,
+			sidx: this._sidx,
 			sord: this._sord,
 			filter: this._filter,
 			nd: Date.now(),
@@ -28,64 +89,8 @@ class TableRequest {
 			}
 		};
 
+	};
 
-	}
+	return DataTableRequest;
 
-
-	set rows(rows) {
-		this._rows = rows;
-	}
-	get rows() {
-		return this._rows;
-	}
-	set page(page) {
-		this._page = page;
-	}
-	get page() {
-		return this._page;
-	}
-	set sixd(sixd) {
-		this._sixd = sixd;
-	}
-	get sixd() {
-		return this._sixd;
-	}
-	set sord(sord) {
-		this._sord = sord;
-	}
-	get sord() {
-		return this._sord;
-	}
-
-	set filter(filter) {
-		this._filter = filter;
-	}
-	get filter() {
-		return this._filter;
-	}
-
-
-
-}
-
-
-class DataTableRequest extends TableRequest {
-
-	constructor(data) {
-		const start = data.start;
-		const length = data.length;
-
-		super(length, (start / length) + 1, 'id', 'asc');
-		this._data = data;
-		this._start = start;
-		this._length = length;
-		this._filter = data.filter;
-	}
-
-
-}
-
-export {
-	TableRequest,
-	DataTableRequest
-};
+}));
--- src/datatable/rup.datatable.js
+++ src/datatable/rup.datatable.js
@@ -1,175 +1,195 @@
-'use strict';
+/*!
+ * Copyright 2014 E.J.I.E., S.A.
+ *
+ * Licencia con arreglo a la EUPL, Versión 1.1 exclusivamente (la «Licencia»);
+ * Solo podrá usarse esta obra si se respeta la Licencia.
+ * Puede obtenerse una copia de la Licencia en
+ *
+ *      http://ec.europa.eu/idabc/eupl.html
+ *
+ * Salvo cuando lo exija la legislación aplicable o se acuerde por escrito,
+ * el programa distribuido con arreglo a la Licencia se distribuye «TAL CUAL»,
+ * SIN GARANTÍAS NI CONDICIONES DE NINGÚN TIPO, ni expresas ni implícitas.
+ * Véase la Licencia en el idioma concreto que rige los permisos y limitaciones
+ * que establece la Licencia.
+ */
 
-import {
-	RupDatatableService
-} from './rup.datatable.service.js';
-import {
-	DataTableRequest
-} from './datatable.request.js';
+/*global define */
+/*global jQuery */
 
+( function( factory ) {
+	if ( typeof define === 'function' && define.amd ) {
 
-import * as datatables from 'datatables.net-bs4';
-import * as datatablesResponsiveBs from 'datatables.net-responsive-bs4';
+		// AMD. Register as an anonymous module.
+		define( ['jquery','./datatable.request','datatables.net-bs4','datatables.net-responsive-bs4'], factory );
+	} else {
 
-import $ from 'jquery';
-
-import plugin from '../utils/jquery.plugin';
-
-
-class RupDatatable {
-
-	constructor(element, options) {
-
-		this._element = element;
-		this._$element = $(element);
-		this._userOptions = options;
-
-		this.options = this._initOptions();
-
-		this.init();
+		// Browser globals
+		factory( jQuery );
 	}
+} ( function( $ , DataTableRequest) {
+
+	//****************************************************************************************************************
+	// DEFINICIÓN BASE DEL PATRÓN (definición de la variable privada que contendrá los métodos y la función de jQuery)
+	//****************************************************************************************************************
+
+	var rup_datatable = {};
+
+	//Se configura el arranque de UDA para que alberge el nuevo patrón
+	$.extend($.rup.iniRup, $.rup.rupSelectorObjectConstructor('rup_datatable', rup_datatable));
+
+	//*******************************
+	// DEFINICIÓN DE MÉTODOS RUP
+	//*******************************
+	$.fn.rup_datatable('extend',{
+		getRupValue: function() {
+			return null;
+		},
+		setRupValue: function(value) {
+
+		}
+	});
+
+	//*******************************
+	// DEFINICIÓN DE MÉTODOS PÚBLICOS
+	//*******************************
+	$.fn.rup_datatable('extend',{
+		foo: function() {
+			return this;
+		}
+	});
+
+	//*******************************
+	// DEFINICIÓN DE MÉTODOS PRIVADOS
+	//*******************************
+
+	$.fn.rup_datatable('extend',{
+		_initOptions: function(options) {
+			var $self = this;
 
 
-	init() {
-		this._$element.DataTable(this.options);
-	}
-
-	_initOptions() {
-
-		const options = Object.assign({}, this._userOptions);
-
-		options.processing = true;
-		options.serverSide = true;
-		options.responsive = true;
-		options.columns = this._getColumns();
+			options.processing = true;
+			options.serverSide = true;
+			options.responsive = true;
+			options.columns = $self._getColumns();
 
 
-		//filter
-		options.filterForm = this._element.previousElementSibling;
-		options.filterButton = options.filterForm.getElementsByTagName('button')[0];
-		options.filterButton.addEventListener('click', () => this._doFilter(options));
-		options.clearButton = options.filterForm.getElementsByClassName('rup-enlaceCancelar')[0];
-		options.clearButton.addEventListener('click', () => this._clearFilter(options));
+			//filter
+
+			// options.filterForm = $self.attr('data-filter-form');
+			options.$filterForm = $(options.filterForm);
+
+			options.$filterButton = options.$filterForm.find('button');
+			options.$clearButton = options.$filterForm.find('.rup-enlaceCancelar');
+			options.$filterButton.on('click', function(){ $self._doFilter(options);});
+			options.$clearButton.on('click', function(){$self._clearFilter(options);});
 
 
 
 
-		// Urls
-		options.urls = {};
-		options.urls.base = this._element.getAttribute('data-url-base');
-		options.urls.filter = options.urls.base + '/filter';
-
-
-
-
-
-
-		options.ajax = this._ajaxOptions();
-
-		options.language = {
-			'url': $.rup.RUP + '/resources/datatable_' + $.rup.lang + '.json'
-		};
-
-
-
-
-		return options;
-
-	}
-
-	_getColumns() {
-		return this._$element.find('th[data-col-prop]').map((i, e) => {
-			return {
-				data: e.getAttribute('data-col-prop')
+			// Urls
+			var baseUrl = options.urlBase;
+			options.urls = {
+				base : baseUrl,
+				filter : baseUrl + '/filter'
 			};
-		});
-	}
-	_doFilter(options) {
 
-		this._$element.DataTable().settings()[0].ajax.filter = form2object(options.filterForm);
-		this._$element.DataTable().ajax.reload();
+			options.ajax = this._ajaxOptions(options);
 
-	}
-
-	_clearFilter(options) {
-
-		// Se realiza el reset de los campos ordinacomorios
-		//form.resetForm();
-		jQuery('input[type!=\'button\'][type!=\'checkbox\'][type!=\'radio\'], textarea', options.filterForm).val('');
-		jQuery('input[type=\'checkbox\']', options.filterForm).not('[name*=\'jqg_GRID_\']', options.filterForm).not('[disabled=\'disabled\']', options.filterForm).removeAttr('checked');
-		// Se realiza el reset de los rup_combo
-		jQuery.each($('select.rup_combo', options.filterForm), function (index, elem) {
-			//if (settings.filter && settings.filter.clearSearchFormMode === 'reset') {
-			//jQuery(elem).rup_combo('reset');
-			//} else {
-			//jQuery(elem).rup_combo('clear');
-			//}
-			jQuery(elem).rup_combo('reset');
-		});
-		//Vaciar los autocompletes
-		$('[ruptype=\'autocomplete\']', options.filterForm).each(function (index, element) {
-			$(element).val('');
-		});
-
-		//Vaciar los arboles
-		$('[ruptype=\'tree\']', options.filterForm).each(function (index, element) {
-			$(element).rup_tree('setRupValue', '');
-		});
-
-
-		$('input[type=\'radio\']', options.filterForm).removeAttr('checked');
-
-		this._doFilter(options);
-
-	}
-
-
-
-	_ajaxOptions() {
-
-		return {
-			'url': this._userOptions.urlBase + '/filter',
-			'dataSrc': this._ajaxSuccessData,
-			'type': 'POST',
-			'data': this._ajaxRequestData,
-			'contentType': 'application/json',
-			'dataType': 'json'
-
-		};
-	}
-
-	_ajaxSuccessData(json) {
-		var ret = {};
-
-		json.recordsTotal = json.records;
-		json.recordsFiltered = json.records;
-
-		ret.recordsTotal = json.records;
-		ret.recordsFiltered = json.records;
-		ret.data = json.rows;
-		return ret.data;
-
-	}
+			options.language = {
+				'url': $.rup.RUP + '/resources/datatable_' + $.rup.lang + '.json'
+			};
 
 
 
 
-	_ajaxRequestData(data, options) {
+			return options;
+		},
+		_getColumns() {
+			return this.find('th[data-col-prop]').map((i, e) => {
+				return {
+					data: e.getAttribute('data-col-prop')
+				};
+			});
+		},
+		_doFilter(options) {
+			var $self = this;
 
-		data.filter = options.ajax.filter;
-		var datatableRequest = new DataTableRequest(data);
-		var json = $.extend({}, data, datatableRequest.getData());
-		return JSON.stringify(json);
+			// $self.DataTable().settings().ajax.filter = form2object(options.$filterForm);
+			$self.DataTable().ajax.reload();
+
+		},
+		_ajaxOptions(options) {
+			var ajaxData = {
+				'url': options.urls.filter,
+				'dataSrc': this._ajaxSuccessData,
+				'type': 'POST',
+				'data': this._ajaxRequestData,
+				'contentType': 'application/json',
+				'dataType': 'json'
+
+			};
 
 
-	}
+			return ajaxData;
+		},
 
-}
+		_ajaxSuccessData(json) {
+			var ret = {};
+
+			json.recordsTotal = json.records;
+			json.recordsFiltered = json.records;
+
+			ret.recordsTotal = json.records;
+			ret.recordsFiltered = json.records;
+			ret.data = json.rows;
+			return ret.data;
+
+		},
 
 
-plugin('rup_datatable', RupDatatable);
 
-export {
-	RupDatatable as
-	default
-};
+
+		_ajaxRequestData(data, options) {
+
+			data.filter = form2object($(options.nTable).data('settings').$filterForm[0]);
+			var datatableRequest = new DataTableRequest(data);
+			var json = $.extend({}, data, datatableRequest.getData());
+			return JSON.stringify(json);
+
+
+		}
+	});
+
+	//*******************************
+	// MÉTODO DE INICIALIZACION
+	//*******************************
+	$.fn.rup_datatable('extend', {
+		_init : function(args){
+			var $self = this,
+				settings = $.extend({}, $.fn.rup_datatable.defaults, $self[0].dataset, args[0]);
+
+			// Se identifica el tipo de componente RUP mediante el valor en el atributo ruptype
+			$self.attr('ruptype', 'datatable');
+
+			$self._initOptions(settings);
+
+
+			$self.DataTable(settings);
+
+			// TODO : Invocación al plugin
+
+			// Se almacena el objeto settings para facilitar su acceso desde los métodos del componente.
+			$self.data('settings', settings);
+
+		}
+	});
+
+	//******************************************************
+	// DEFINICIÓN DE LA CONFIGURACION POR DEFECTO DEL PATRON
+	//******************************************************
+	$.fn.rup_datatable.defaults = {
+		foobar: false
+	};
+
+}));
