af6cc313baf5619e4b9bf884ad4e20efd6e8fffa
ecarrasson <ecarrasson@euro-help.es>
2018-03-22 12:54:42 +0100

    TEST Dialog Initial Commit

--- spec/dialog/rup.dialog.spec.js
+++ spec/dialog/rup.dialog.spec.js
@@ -1,182 +1,83 @@
-//
-// (function (factory) {
-//       if (typeof define === "function" && define.amd) {
-//
-//           // AMD. Register as an anonymous module.
-//           define(['jquery','handlebars','rup.dialog'], factory);
-//       } else {
-//
-//           // Browser globals
-//           factory(jQuery);
-//       }
-//   }(function ($, Handlebars) {
-
 import 'jquery';
 import 'handlebars';
 import 'jasmine-jquery';
 import 'rup.dialog';
 
-function executeSharedTests(createInstanceFn){
-
-
-	describe('Construcción de un RUP Dialog', function(){
-		var $message, $messageTitleDiv, $messageContentDiv, $messageButtonpaneDiv;
-
-		beforeAll(function(){
-			$message = createInstanceFn();
-
-			$messageTitleDiv = jQuery('.ui-dialog-titlebar', $message);
-			$messageContentDiv = jQuery('.ui-dialog-content', $message);
-			$messageButtonpaneDiv = jQuery('.ui-dialog-buttonpane', $message);
-
-		});
-
-		it('deberia de existir un ui-dialog', function(){
-			expect($message).toExist();
-		});
-
-		describe('Se ha construido una sección de título correcta', function(){
-			it('deberia de existir una sección de título', function(){
-				expect($messageTitleDiv).toExist();
+describe('Test Dialog', () => {
+	function testDialogType(type) {
+		describe('Test de creacion', () => {
+			var $dialogo;
+			beforeAll( () => {
+				var html = '<div id="exampleDialogo"></div>'
+				$('body').append(html);
+				let opciones = {
+					type: type,
+					autoOpen: false,
+					width: 200,
+					title: 'TituloDialogo',
+					message: 'MensajeDialogo'
+				};
+				$dialogo = $('#exampleDialogo');
+				$dialogo.rup_dialog(opciones);
 			});
-
-			it('deberia de existir una botón de cerrar', function(){
-				expect($('.ui-dialog .ui-dialog-titlebar button').is('.ui-button.ui-corner-all.ui-widget.ui-button-icon-only.ui-dialog-titlebar-close')).toBe(true);
-			});
-
-			it('deberia de existir un texto de cerrar', function(){
-				expect($('.ui-dialog .ui-dialog-titlebar a.ui-dialog-title span')).toExist();
-				expect($('.ui-dialog .ui-dialog-titlebar a.ui-dialog-title span')).toHaveText('cerrar');
+			it('debe existir:', () => {
+				expect($('.ui-dialog')).toExist();
 			});
 		});
-
-		describe('Construcción de la botonera', function(){
-			it('deberia de existir una sección que contenga los botones', function(){
-				expect($messageButtonpaneDiv).toExist();
+		describe('Métodos públicos:', () => {
+			describe('Método open e isOpen', () => {
+				beforeAll( () => {
+					$dialogo.rup_dialog('open');
+				});
+				it('Debe ser visible:', () => {
+					expect($dialogo.css('display')).toBe('block');
+				});
+				describe('Método isOpen:', () => {
+					it('debe estar abierto:',  () => {
+						expect($dialogo.rup_dialog('isOpen')).toBe(true);
+					});
+				});
 			});
-
-			it('deberia de existir al menos el botón de Aceptar', function(){
-				expect($('.ui-dialog-buttonset button',$messageButtonpaneDiv)).toHaveText('Aceptar');
+			describe('Método close e isOpen', () => {
+				beforeAll( () => {
+					$dialogo.rup_dialog('close');
+				});
+				it('No debe ser visible:', () => {
+					expect($dialogo.css('display')).toBe('none');
+				});
+				it('No debe estar abierto', () => {
+					expect($dialogo.rup_dialog('isOpen')).toBe(false);
+				});
 			});
-		});
-
-
-	});
-
-}
-
-describe('RUP Dialog Tests', function(){
-
-
-	describe('Mostrar un mensaje de error', function(){
-
-		function createInstance (){
-			$.rup_messages('msgError', {
-				title: 'Error',
-				message: 'Se ha producido un error.'
+			describe('Método widget', () => {
+				it('No debe devolver undefined', () => {
+					expect($dialogo.rup_dialog('widget')).toBeDefined();
+				});
 			});
-
-			return jQuery('.ui-dialog');
-		}
-
-
-		executeSharedTests(createInstance);
-		afterAll(function(){
-			$('.ui-dialog>.ui-dialog-content').dialog('destroy').remove();
-		});
-
-		it('debería de existir un div con el icono de error', function(){
-			expect($('.ui-dialog>.ui-dialog-content > div.rup-message_icon-error')).toExist();
-		});
-
-	});
-
-	describe('Mostrar un mensaje de alert', function(){
-
-		function createInstance (){
-			$.rup_messages('msgAlert', {
-				title: 'Alerta',
-				message: 'Esto es un mensaje de alerta .'
+			describe('Metodo moveToTop', () => {
+				it('No debe lanzar error', () => {
+					expect($dialogo.rup_dialog('moveToTop')).not.toThrowError();
+				});
 			});
-
-			return jQuery('.ui-dialog');
-		}
-
-		afterAll(function(){
-			$('.ui-dialog>.ui-dialog-content').dialog('destroy').remove();
-		});
-
-		executeSharedTests(createInstance);
-
-		it('debería de existir un div con el icono de alerta', function(){
-			expect($('.ui-dialog>.ui-dialog-content > div.rup-message_icon-alert')).toExist();
-		});
-
-
-	});
-
-	describe('Mostrar un mensaje de Ok', function(){
-
-		function createInstance (){
-			$.rup_messages('msgOK', {
-				title: 'Correcto',
-				message: 'Todo ha ido Ok'
+			describe('Método getOption', () => {
+				it('Obtiene la opcion correctamente', () => {
+					expect($dialogo.rup_dialog('getOption','width')).toBe(200);
+				});
 			});
-
-			return jQuery('.ui-dialog');
-		}
-
-		afterAll(function(){
-			$('.ui-dialog>.ui-dialog-content').dialog('destroy').remove();
-		});
-
-		executeSharedTests(createInstance);
-
-		it('debería de existir un div con el icono de Ok', function(){
-			expect($('.ui-dialog>.ui-dialog-content > div.rup-message_icon-ok')).toExist();
-		});
-
-
-	});
-
-	describe('Mostrar un mensaje de confirmación', function(){
-		var callbacks={
-			fncOkFunction: function(){
-				alert('asdasd');}
-		};
-
-		function createInstance (){
-			$.rup_messages('msgConfirm', {
-				message: '¿Está seguro que desea cancelar?',
-				title: 'Confirmación',
-				OKFunction : callbacks.fncOkFunction
+			describe('Método setOption', () => {
+				beforeAll(() => {
+					$dialogo.rup_dialog('setOption', 'width', 260);
+				});
+				it('Establece la opcion correctamente', () => {
+					expect($dialogo.rup_dialog('getOption','width')).toBe(260);
+				});
 			});
-
-			return jQuery('.ui-dialog');
-		}
-
-		beforeEach(function() {
-			spyOn(callbacks, 'fncOkFunction');
+			
+			generalFunc($dialogo,[enable,disable,destroy]);
 		});
+	}
+	
+	testDialogType($.rup.dialog.TEXT);
+	testDialogType($.rup.dialog.DIV);
 
-		afterAll(function(){
-			$('.ui-dialog>.ui-dialog-content').dialog('destroy').remove();
-		});
-
-		executeSharedTests(createInstance);
-
-		it('debería de existir un div con el icono de confirmación', function(){
-			expect($('.ui-dialog>.ui-dialog-content > div.rup-message_icon-confirm')).toExist();
-		});
-
-		it('debería de existir un enlace en la botonera que permita cancelar', function(){
-			expect($('.ui-dialog .ui-dialog-buttonset a.rup-enlaceCancelar')).toHaveText(jQuery.rup.i18nParse(jQuery.rup.i18n.base,'rup_global.cancel'));
-		});
-		// FIXME
-		// it("debería de ejecutarse el callback del botón de aceptar", function(){
-		//     $(".ui-dialog .ui-dialog-buttonset button:visible").click()
-		//     expect(callbacks.fncOkFunction).toHaveBeenCalled();
-		// });
-	});
 });
-// }));
