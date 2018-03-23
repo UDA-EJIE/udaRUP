25442fa2627ebe696bfce4f95c8e587ba605f5b0
ecarrasson <ecarrasson@euro-help.es>
2018-03-23 09:58:08 +0100

    Revisado parece correccto, Habría que probarlo.

--- spec/autocomplete/rup.autocomplete.spec.js
+++ spec/autocomplete/rup.autocomplete.spec.js
@@ -37,27 +37,26 @@
 			$autocomplete = $('#idAutocomplete');
 	
 			var sourceJson = [
-				{i18nCaption: 'asp', value:'asp_value'},
-				{i18nCaption: 'c', value:'c_value'},
-				{i18nCaption: 'c++', value:'c++_value'},
+				{i18nCaption: 'asp',        value:'asp_value'},
+				{i18nCaption: 'c',          value:'c_value'},
+				{i18nCaption: 'c++',        value:'c++_value'},
 				{i18nCaption: 'coldfusion', value:'coldfusion_value'},
-				{i18nCaption: 'groovy', value:'groovy_value'},
-				{i18nCaption: 'haskell', value:'haskell_value'},
-				{i18nCaption: 'java', value:'java_value'},
+				{i18nCaption: 'groovy',     value:'groovy_value'},
+				{i18nCaption: 'haskell',    value:'haskell_value'},
+				{i18nCaption: 'java',       value:'java_value'},
 				{i18nCaption: 'javascript', value:'javascript_value'},
-				{i18nCaption: 'perl', value:'perl_value'},
-				{i18nCaption: 'php', value:'php_value'},
-				{i18nCaption: 'python', value:'python_value'},
-				{i18nCaption: 'ruby', value:'ruby_value'},
-				{i18nCaption: 'scala', value:'scala_value'}
+				{i18nCaption: 'perl',       value:'perl_value'},
+				{i18nCaption: 'php',        value:'php_value'},
+				{i18nCaption: 'python',     value:'python_value'},
+				{i18nCaption: 'ruby',       value:'ruby_value'},
+				{i18nCaption: 'scala',      value:'scala_value'}
 			];
 	
 			$autocomplete.rup_autocomplete({
-				source : sourceJson,
+				source :       sourceJson,
 				defaultValue : 'java',
-				contains : false
+				contains :     false
 			});
-	
 	
 		});
 	
