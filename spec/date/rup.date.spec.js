81f28b579264297a7bab5106e01a5aea427d03cb
ecarrasson <ecarrasson@euro-help.es>
2018-03-27 10:26:49 +0200

    Desarrollo finalizado (Falta añadir archivo generalFunc) hay que probarlo.

--- spec/date/rup.date.spec.js
+++ spec/date/rup.date.spec.js
@@ -32,9 +32,63 @@ describe('TEST Date:',   () => {
             });
         });
         describe('Métodos hide y show', () => {
-            describe('Método hide', () => {});//Esconde el input dejando el boton
-            describe('Método show', () => {});//Muestra el input escondido
+            describe('Método hide', () => {
+                beforeAll(() => {
+                    $date.rup_date('hide');
+                });
+                it('Debe estar oculto', () => {
+                    expect($('.ui-datepicker').css('display')).toBe('none');
+                });
+            });
+            describe('Método show', () => {
+                beforeAll(() => {
+                    $date.rup_date('show');
+                });
+                it('Debe ser visible', () => {
+                    expect($('.ui-datepicker').css('display')).toBe('block');
+                });
+            });
         });
+        describe('Métodos setDate y getDate', () => {
+            beforeAll(() =>{
+                $date.rup_date('setDate', new Date('01-01-2018'));
+            });
+            describe('Método setDate', () => {
+                it('Debe cambiar el valor de input a lo establecido', () => {
+                    expect($date.val()).toBe('01/01/2018');
+                });
+            });
+            describe('Método getDate', () => {
+                it('Debe obtener el mismo valor que se ha mostrado', () => {
+                    expect($date.rup_date('getDate')).toBe($date.val());
+                });
+            });
+        });
+        describe('Método option', () => {
+            describe('Establecer una propiedad y que se obtenga el valor cambiado', () => {
+                afterAll(() => {
+                    $date.rup_date('option','disabled', false);
+                });
+                it('No debe haber errores al establecer el valor de la propiedad', () => {
+                    expect($date.rup_date('option','disabled', true)).not.toThrowError();
+                });
+                it('Al obtener el valor de la propiedad este debe ser el establecido anteriormente', () => {
+                    expect($date.rup_date('option', 'disabled')).toBeTruthy();
+                });
+            });
+            describe('Establecer varias propiedades', () => {
+                afterAll();
+                it('No debe lanzar errores en la asignacion', () => {
+                    expect($date.rup_date('option', {autoSize:true, showWeek:true}));
+                });
+                it('Los valores deben estar actualizados', () => {
+                    expect($date.rup_date('option','autoSize')).toBeTruthy();
+                    expect($date.rup_date('option','showWeek')).toBeTruthy();
+                })
+            });
+        });
+
+        generalFunc($date, 'rup_date', ['getRupValue','setRupValue','enable','disable','destroy']);
         
     });
 });
