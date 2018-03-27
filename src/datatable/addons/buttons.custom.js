1b869c0776356a357d8d29e9260f5c49c17f3044
Xabi <xabierportu@gmail.com>
2018-03-27 16:01:17 +0200

    Botón de copia finalizado, mejorado y documentado
    El botón encargado de la copia de registros al portapapeles ha sido finalizado y mejoradas algunas de sus funciones. También ha sido documentado y algunas líneas han sido comentadas para así entender mejor su cometido.

--- src/datatable/addons/buttons.custom.js
+++ src/datatable/addons/buttons.custom.js
@@ -1,60 +1,285 @@
 /*!
- * TODO
+ * Copyright 2018 E.J.I.E., S.A.
+ *
+ * Licencia con arreglo a la EUPL, Versión 1.1 exclusivamente (la «Licencia»);
+ * Solo podrá usarse esta obra si se respeta la Licencia.
+ * Puede obtenerse una copia de la Licencia en
+ *
+ *      http://ec.europa.eu/idabc/eupl.html
+ *
+ * Salvo cuando lo exija la legislación aplicable o se acuerde por escrito,
+ * el programa distribuido con arreglo a la Licencia se distribuye «TAL CUAL»,
+ * SIN GARANTÍAS NI CONDICIONES DE NINGÚN TIPO, ni expresas ni implícitas.
+ * Véase la Licencia en el idioma concreto que rige los permisos y limitaciones
+ * que establece la Licencia.
  */
 
-(function( factory ){
-	if ( typeof define === 'function' && define.amd ) {
+(function(factory){
+	if (typeof define === 'function' && define.amd) {
 		// AMD
-		define( ['jquery', 'datatables.net'], function ( $ ) {
-			return factory( $, window, document );
-		} );
+		define(['jquery', 'datatables.net'], function ($) {
+			return factory($, window, document);
+		});
 	}
-	else if ( typeof exports === 'object' ) {
+	else if (typeof exports === 'object') {
 		// CommonJS
 		module.exports = function (root, $) {
-			if ( ! root ) {
+			if (! root) {
 				root = window;
 			}
 
-			if ( ! $ || ! $.fn.dataTable ) {
+			if (! $ || ! $.fn.dataTable) {
 				$ = require('datatables.net')(root, $).$;
 			}
 
-			return factory( $, root, root.document );
+			return factory($, root, root.document);
 		};
 	}
 	else {
 		// Browser
-		factory( jQuery, window, document );
+		factory(jQuery, window, document);
 	}
-}(function( $, window, document ) {
+}(function($, window, document) {
 'use strict';
 var DataTable = $.fn.dataTable;
-
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * Local (private) functions
  */
 
+
 /**
-* TODO: descripcion
-*
+	* Establece el tipo de llamada necesario para obtener los datos segun lo seleccionado
+	* e inicia la gestion para finalmente obtenerlos
+	* @name _reportsCopyData
+	* @function
+	* @private
+	* @param {object} dt Instancia del datatable
+	* @param {object} that Objeto del boton
+	* @param {object} config Configuracion del boton
+*/
+var _reportsCopyData = function (dt, that, config)
+{
+	var ctx = dt.settings()[0];
+	var info = dt.buttons.exportInfo(config);
+	var type;
+	var multiselection = DataTable.multiSelect.multiselection;
+	var selectedAll = multiselection.selectedAll;
+	var deselectedIds = multiselection.deselectedIds;
+
+	if (selectedAll) {
+		if (deselectedIds.length > 0) {
+			// Este caso es para cuando se selecciona todo y despues se
+			// deseleccionan algunos registros
+			type = "all-deselected";
+		} else {
+			// Este caso es para cuando se seleccionan todos los registros
+			type = "all";
+		}
+	} else {
+		// Este caso para cuando hay determinados registros seleccionados manualmente
+		type = "selected";
+	}
+
+	$.when(_reportsTypeOfCopy(dt, type, multiselection, selectedAll, deselectedIds)).then(function (exportData) {
+		var exportDataRows = exportData.length;
+		var exportDataParsed = JSON.stringify(exportData);
+
+		var hiddenDiv = $('<div/>')
+			.css({
+				height: 1,
+				width: 1,
+				overflow: 'hidden',
+				position: 'fixed',
+				top: 0,
+				left: 0
+			});
+
+		var textarea = $('<textarea readonly/>')
+			.val(exportDataParsed)
+			.appendTo(hiddenDiv);
+
+		_reportsOpenMessage(dt, ctx, that, exportDataRows, hiddenDiv, textarea);
+	});
+};
+
+/**
+	* Segun el tipo de funcion de copia solicitada, realiza unas u otras comprobaciones
+	* antes de solicitar los datos al servidor
+	* @name _reportsTypeOfCopy
+	* @function
+	* @private
+	* @param {object} dt Instancia del datatable
+	* @param {string} type Tipo de funcion de copia a ejecutar
+	* @param {object} multiselection Propiedades de la multiseleccion
+	* @param {boolean} selectedAll Cuando es true significa que todas las filas estan marcadas
+	* @param {array} [deselectedIds] ID's de las filas deseleccionadas
+	* @return {object}
+*/
+var _reportsTypeOfCopy = function (dt, type, multiselection, selectedAll, deselectedIds)
+{
+	var ctx = dt.settings()[0];
+	var deferred = $.Deferred();
+	var exportData;
+	var selectedIds = multiselection.selectedIds;
+	var selectedRows = multiselection.selectedRowsPerPage;
+	var ajaxOptions = {};
+	var urlAjax;
+	var typeAjax;
+	var contentTypeAjax = 'application/json';
+	var dataTypeAjax = 'json';
+
+	switch (type) {
+		case 'selected':
+			var localAccess = true;
+			var exportData = [];
+
+			// Comprueba si todos los valores seleccionados estan en la misma pagina
+			$.each(selectedRows, function(key, value) {
+				if (ctx.json.page != value.page) {
+					localAccess = false;
+					return false;
+				}
+			});
+			if (localAccess) {
+				// Puede acceder a los valores seleccionados localmente
+				$.each(selectedRows, function(key, value) {
+					var idPadre = value.id;
+					$.each(ctx.json.rows, function(key, value) {
+						if (value.id === idPadre) {
+							exportData.push(value);
+						}
+					});
+				});
+				deferred.resolve(exportData);
+			} else {
+				// Accede a los datos mediante el servidor ya que se ha hecho uso de la paginacion
+				// Parametros necesarios para configurar la llamada AJAX
+				urlAjax = '/clipboardReport';
+				typeAjax = 'POST';
+				ajaxOptions = _reportsPrepareRequestData(ajaxOptions, urlAjax, typeAjax, contentTypeAjax, dataTypeAjax, ctx, selectedAll, deselectedIds, selectedIds);
+
+				$.when(_reportsRequestData(ajaxOptions)).then(function (data) {
+					exportData = data;
+					deferred.resolve(exportData);
+				});
+			}
+			break;
+		case 'all':
+			// Parametros necesarios para configurar la llamada AJAX
+			typeAjax = 'GET';
+			ajaxOptions = _reportsPrepareRequestData(ajaxOptions, urlAjax, typeAjax, contentTypeAjax, dataTypeAjax, ctx, selectedAll, deselectedIds, selectedIds);
+
+			$.when(_reportsRequestData(ajaxOptions)).then(function (data) {
+				ctx.oInit.buttons.allData = data;
+				exportData = ctx.oInit.buttons.allData;
+				deferred.resolve(exportData);
+			});
+			break;
+		case 'all-deselected':
+			// Parametros necesarios para configurar la llamada AJAX
+			urlAjax = '/clipboardReport';
+			typeAjax = 'POST';
+			ajaxOptions = _reportsPrepareRequestData(ajaxOptions, urlAjax, typeAjax, contentTypeAjax, dataTypeAjax, ctx, selectedAll, deselectedIds, selectedIds);
+
+			$.when(_reportsRequestData(ajaxOptions)).then(function (data) {
+				exportData = data;
+				deferred.resolve(exportData);
+			});
+			break;
+	}
+
+	return deferred.promise();
+};
+
+/**
+	* Se encarga de generar las opciones de configuracion con las que se llamara a la API
+	* @name _reportsPrepareRequestData
+	* @function
+	* @param {object} ajaxOptions Parametros de la llamada Ajax
+	* @param {string} urlAjax Parametro para la URL
+	* @param {string} typeAjax Tipo de llamada a la API
+	* @param {string} contentTypeAjax Formato de datos enviados
+	* @param {string} dataTypeAjax Formato de datos esperados
+	* @param {object} ctx Contexto
+	* @param {boolean} selectedAll Cuando es true significa que todas las filas estan marcadas
+	* @param {array} [deselectedIds] ID's de las filas deseleccionadas
+	* @param {array} [selectedIds] ID's de las filas seleccionadas
+	* @private
+	* @return {object}
+ */
+var _reportsPrepareRequestData = function (ajaxOptions, urlAjax, typeAjax, contentTypeAjax, dataTypeAjax, ctx, selectedAll, deselectedIds, selectedIds)
+{
+	var row = {};
+	row.core =  {
+		'pkToken': ctx.oInit.multiplePkToken,
+		'pkNames': ctx.oInit.primaryKey
+	};
+	row.multiselection = {};
+	row.multiselection.selectedAll = selectedAll;
+	if (row.multiselection.selectedAll) {
+		row.multiselection.selectedIds = deselectedIds;
+	} else {
+		row.multiselection.selectedIds = selectedIds;
+	}
+	// Completa el objeto 'ajaxOptions' con los parametros necesarios para la
+	// llamada que se realizara al servidor
+	ajaxOptions.contentType = contentTypeAjax;
+	ajaxOptions.dataType = dataTypeAjax;
+	if (urlAjax !== undefined) {
+		ajaxOptions.url = ctx.oInit.urlBase + urlAjax;
+	} else {
+		ajaxOptions.url = ctx.oInit.urlBase;
+	}
+	ajaxOptions.type = typeAjax;
+	if (typeAjax === 'POST') {
+			ajaxOptions.data = JSON.stringify(row);
+	}
+
+	return ajaxOptions;
+};
+
+/**
+	* Se encarga de llamar a la API y de devolver los datos recibidos
+	* @name _reportsRequestData
+	* @function
+	* @private
+	* @param {object} ajaxOptions Parametros de la llamada Ajax
+	* @return {object}
+ */
+var _reportsRequestData = function (ajaxOptions)
+{
+	var deferred = $.Deferred();
+	$.ajax(ajaxOptions)
+		.done(function(data) {
+			deferred.resolve(data);
+		});
+	return deferred.promise();
+};
+
+/**
+	* Gestiona la apertura/cierre del mensaje de confirmación de copia
+	* @name _reportsOpenMessage
+	* @function
+	* @private
+	* @param {object} dt Instancia del datatable
+	* @param {object} ctx Contexto
+	* @param {object} that Objeto del boton
+	* @param {int} exportDataRows Numero de filas a ser exportadas
+	* @param {object} hiddenDiv Elemento del DOM
+	* @param {object} textarea Elemento del DOM
 */
 var _reportsOpenMessage = function (dt, ctx, that, exportDataRows, hiddenDiv, textarea)
 {
 	$.rup_messages('msgConfirm', {
-		title: dt.i18n( 'changes', 'Copia de registros en clipboard' ),
-		message: dt.i18n( 'saveAndContinue', {
+		title: dt.i18n('changes', 'Copia de registros en clipboard'),
+		message: dt.i18n('saveAndContinue', {
 			1: '¿Desea copiar un registro?',
 			_: '¿Desea copiar %d registros?'
-
-		}, exportDataRows ),
+		}, exportDataRows),
 		OKFunction: function () {
 			ctx.oInit.formEdit.okCallBack = true;
-
-			// TODO: controlar los diferentes casos de uso
-			_reportsCopyAllDataToClipBoard(dt, that, exportDataRows, hiddenDiv, textarea);
-
+			_reportsCopyDataToClipboard(dt, that, exportDataRows, hiddenDiv, textarea);
 			ctx.oInit.formEdit.detailForm.rup_dialog("close");
 		},
 		beforeClose: function (){
@@ -62,151 +287,48 @@ var _reportsOpenMessage = function (dt, ctx, that, exportDataRows, hiddenDiv, te
 			// Si es llamado desde el contextMenu este paso es innecesario y la condicion
 			// del if evita un error
 			if (that.processing !== undefined) {
-				that.processing( false );
+				that.processing(false);
 			}
 		}
 	});
 };
 
 /**
- * TODO: descripcion
- *
+	* Copia los datos recibidos al portapapeles
+	* @name _reportsCopyDataToClipboard
+	* @function
+	* @private
+	* @param {object} dt Instancia del datatable
+	* @param {object} that Objeto del boton
+	* @param {int} exportDataRows Numero de filas a ser exportadas
+	* @param {object} hiddenDiv Elemento del DOM
+	* @param {object} textarea Elemento del DOM
  */
-var _reportsRequestAllData = function ( ctx )
-{
-	var deferred = $.Deferred();
-	$.ajax({
-		method: "GET",
-		dataType: "json",
-		url: ctx.oInit.urlBase
-	})
-		.done(function( data ) {
-			deferred.resolve(data);
-		});
-	return deferred.promise();
-};
-
-/**
- * TODO: descripcion
- *
- */
-var _reportsTypeOfCopy = function (ctx, type, selectedRows)
-{
-	var deferred = $.Deferred();
-	var exportData;
-
-	switch (type) {
-		case 'selected':
-			console.log("SELECTED");
-			exportData = selectedRows;
-			deferred.resolve(exportData);
-			break;
-		case 'all':
-			console.log("ALL");
-			$.when(_reportsRequestAllData(ctx)).then(function (data) {
-				ctx.oInit.buttons.allData = data;
-				exportData = ctx.oInit.buttons.allData;
-				deferred.resolve(exportData);
-			});
-			break;
-		// TODO: hacer el case que necesitara un filtro
-		case 'all-deselected':
-			console.log("ALL - DESELECTED");
-
-			//////////////// PROBANDO ////////////////
-			console.log("////////////////////////////////");
-			var datos = $self._ajaxRequestData;
-			var ajaxOptions = {
-				url : ctx.oInit.urlBase,
-				usePlugins:["filter"],
-				filter:{
-					// Propiedades de configuración del plugin filter
-					id: ["05"],
-					nombre: ["as"],
-					fechaAlta: ["13/03/2018"]
-				},
-				success : function(data, status, xhr) {
-					console.log("SUCCESS");
-					deferred.resolve(data);
-					console.log(data);
-				},
-				error : function(xhr, ajaxOptions,thrownError) {
-					console.log('Errors '+thrownError+ ": "+xhr.responseText);
-
-				}
-			};
-			$('#id_filter_table').rup_form('ajaxSubmit', ajaxOptions);
-			console.log("////////////////////////////////");
-			//////////////// PROBANDO ////////////////
-
-			break;
-		default:
-			// TODO: falta dar accion por defecto
-			console.log("DEFAULT");
-	}
-
-	return deferred.promise();
-};
-
-/**
- * TODO: descripcion
- *
- */
-var _reportsCopyData = function ( dt, that, config, type, selectedIds, selectedRows )
-{
-	var ctx = dt.settings()[0];
-	var info = dt.buttons.exportInfo( config );
-
-	$.when(_reportsTypeOfCopy(ctx, type, selectedRows)).then(function (exportData) {
-		var exportDataRows = exportData.length;
-
-		var hiddenDiv = $('<div/>')
-			.css( {
-				height: 1,
-				width: 1,
-				overflow: 'hidden',
-				position: 'fixed',
-				top: 0,
-				left: 0
-			} );
-
-		var textarea = $('<textarea readonly/>')
-			.val( exportData )
-			.appendTo( hiddenDiv );
-
-		_reportsOpenMessage(dt, ctx, that, exportDataRows, hiddenDiv, textarea);
-	});
-};
-
-/**
- * TODO: descripcion
- *
- */
-var _reportsCopyAllDataToClipBoard = function ( dt, that, exportDataRows, hiddenDiv, textarea )
+var _reportsCopyDataToClipboard = function (dt, that, exportDataRows, hiddenDiv, textarea)
 {
 	// For browsers that support the copy execCommand, try to use it
-	if ( document.queryCommandSupported('copy') ) {
-		hiddenDiv.appendTo( dt.table().container() );
+	if (document.queryCommandSupported('copy')) {
+		hiddenDiv.appendTo(dt.table().container());
 		textarea[0].focus();
 		textarea[0].select();
 
 		try {
-			var successful = document.execCommand( 'copy' );
+			var successful = document.execCommand('copy');
 			hiddenDiv.remove();
 
 			if (successful) {
 				dt.buttons.info(
-					dt.i18n( 'changes', 'Copia de registros en clipboard' ),
-					dt.i18n( 'saved', {
+					dt.i18n('changes', 'Copia de registros en clipboard'),
+					dt.i18n('saved', {
 						1: 'Copiado un registro al portapapeles',
 						_: 'Copiados %d registros al portapapeles'
-					}, exportDataRows ),
+					}, exportDataRows),
 					2000
 				);
 				// Si es llamado desde el contextMenu este paso es innecesario y la condicion
 				// del if evita un error
 				if (that.processing !== undefined) {
-					that.processing( false );
+					that.processing(false);
 				}
 				return;
 			}
@@ -215,13 +337,13 @@ var _reportsCopyAllDataToClipBoard = function ( dt, that, exportDataRows, hidden
 	}
 
 	// Otherwise we show the text box and instruct the user to use it
-	var message = $('<span>'+dt.i18n( 'copyKeys',
+	var message = $('<span>'+dt.i18n('copyKeys',
 		'Presiona <i>ctrl</i> o <i>\u2318</i> + <i>C</i> para copiar los datos de la tabla<br>al portapapeles.<br><br>'+
-    'Para cancelar, haz click sobre este mensaje o pulsa el botón escape.' )+'</span>'
+    'Para cancelar, haz click sobre este mensaje o pulsa el botón escape.')+'</span>'
 	)
-	.append( hiddenDiv );
+	.append(hiddenDiv);
 
-	dt.buttons.info( dt.i18n( 'copyTitle', 'Copiar al portapapeles' ), message, 0 );
+	dt.buttons.info(dt.i18n('copyTitle', 'Copiar al portapapeles'), message, 0);
 
 	// Select the text so when the user activates their system clipboard
 	// it will copy that text
@@ -231,33 +353,32 @@ var _reportsCopyAllDataToClipBoard = function ( dt, that, exportDataRows, hidden
 	// Event to hide the message when the user is done
 	var container = $(message).closest('.dt-button-info');
 	var close = function () {
-		container.off( 'click.buttons-copy' );
-		$(document).off( '.buttons-copy' );
-		dt.buttons.info( false );
+		container.off('click.buttons-copy');
+		$(document).off('.buttons-copy');
+		dt.buttons.info(false);
 		// Si es llamado desde el contextMenu este paso es innecesario y la condicion
 		// del if evita un error
 		if (that.processing !== undefined) {
-			that.processing( false );
+			that.processing(false);
 		}
 	};
 
-	container.on( 'click.buttons-copy', close );
+	container.on('click.buttons-copy', close);
 	$(document)
-		.on( 'keydown.buttons-copy', function (e) {
-			if ( e.keyCode === 27 ) { // esc
+		.on('keydown.buttons-copy', function (e) {
+			if (e.keyCode === 27) { // esc
 				close();
 			}
-		} )
-		.on( 'copy.buttons-copy cut.buttons-copy', function () {
+		})
+		.on('copy.buttons-copy cut.buttons-copy', function () {
 			close();
 			// Si es llamado desde el contextMenu este paso es innecesario y la condicion
 			// del if evita un error
 			if (that.processing !== undefined) {
-				that.processing( false );
+				that.processing(false);
 			}
-		} );
+		});
 };
-
 
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
@@ -268,52 +389,24 @@ var _reportsCopyAllDataToClipBoard = function ( dt, that, exportDataRows, hidden
 // Copy to clipboard
 //
 DataTable.ext.buttons.copyCustom = {
-	text: function ( dt ) {
-		return dt.i18n( 'toolbar.reports.copyCustom', 'Copiar' );
+	text: function (dt) {
+		return dt.i18n('toolbar.reports.copyCustom', 'Copiar');
 	},
 	className: 'buttons-copyCustom',
 	displayRegex: /^[1-9][0-9]*$/, // Se muestra siempre que sea un numero mayor a 0
 	insideContextMenu: true,
 	type: 'copyCustom',
-	init: function ( dt, node, config ) {
+	init: function (dt, node, config) {
 		DataTable.ext.buttons.copyCustom.eventDT = dt;
 	},
-	action: function ( e, dt, button, config ) {
+	action: function (e, dt, button, config) {
 		// Si es llamado desde el contextMenu este paso es innecesario y la condicion
 		// del if evita un error
 		if (this.processing !== undefined) {
-			this.processing( true );
+			this.processing(true);
 		}
-
 		var that = this;
-		var multiselection = DataTable.multiSelect.multiselection;
-		var selectedRows = multiselection.selectedRowsPerPage;
-		var numOfSelectedRows = multiselection.numSelected;
-		var selectedAll = multiselection.selectedAll;
-		var selectedIds = multiselection.selectedIds;
-		var deselectedIds = multiselection.deselectedIds;
-
-		if (selectedAll) {
-			console.log("SELECTALL = TRUE");
-			if (deselectedIds.length > 0) {
-				// TODO: Este caso es para cuando se selecciona todo y despues se
-				// deseleccionan algunos registros
-				console.log("TODO - ALGO");
-				console.log(deselectedIds);
-				var type = "all-deselected";
-				_reportsCopyData(dt, that, config, type, selectedIds, selectedRows);
-			} else {
-				// Este caso es para cuando se seleccionan todos los registros
-				console.log("TODO");
-				var type = "all";
-				_reportsCopyData(dt, that, config, type, selectedIds, selectedRows);
-			}
-		} else {
-			// Este caso para cuando hay determinados registros seleccionados manualmente
-			console.log("SELECTALL = FALSE");
-			var type = "selected";
-			_reportsCopyData(dt, that, config, type, selectedIds, selectedRows);
-		}
+		_reportsCopyData(dt, that, config);
 	}
 };
 
