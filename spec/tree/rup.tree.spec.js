fde69acdcf900b98b3fb0a997b28f232cc0c4aed
ecarrasson <ecarrasson@euro-help.es>
2018-04-06 14:54:52 +0200

    Test Tree: Initial Commit

---
+++ spec/tree/rup.tree.spec.js
@@ -0,0 +1,35 @@
+import 'jquery';
+
+import 'jasmine-jquery';
+import 'rup.tree';
+
+describe('Test Tree:', () => {
+    var $tree;
+    describe('Creación', () => {
+        var html = '<div id="exampleTree">'
+                +       '<ul>'
+                +           '<li id="node1">'
+                +               '<a href = "#">Padre</a>'
+                +               '<ul>'
+                +                   '<li id="node11">'
+                +                       '<a href="#">Hijo 1</a>'
+                +                   '</li>'
+                +                   '<li id="node12">'
+                +                       '<a href="#">Hijo 2</a>'
+                +                   '</li>'
+                +               '</ul>'
+                +           '</li>'
+                +       '</ul>'
+                +  '</div>'
+        $('body').append(html);
+        $('exampleTree').rup_tree({});
+        $tree = $('exampleTree');
+
+        it('Debe tener el attr ruptype = tree', () => {
+            expect($tree.attr('ruptype')).toBe('tree');
+        });
+    });
+    describe('Métodos públicos:', () => {
+        generalFunc($tree, 'rup_tree', ['getRupValue','setRupValue']);
+    });
+});
