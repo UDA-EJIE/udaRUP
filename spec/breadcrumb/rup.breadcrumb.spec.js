7b8438b42dae4bc50e9e30444d641d1e92f38602
ecarrasson <ecarrasson@euro-help.es>
2018-04-12 16:08:28 +0200

    refactor (FIXME)

--- spec/breadcrumb/rup.breadcrumb.spec.js
+++ spec/breadcrumb/rup.breadcrumb.spec.js
@@ -3,36 +3,31 @@ import 'jasmine-jquery';
 import 'rup.breadcrumb';
 
 describe('RUP BreadCrumb Test:', () => {
-  var $breadcrumb;
-  describe('Creación del elemento: ', () => {
-    var html;
+	var $breadcrumb;
+	describe('Creación del elemento: ', () => {
+		var html;
 
-    beforeAll(() => {
-      html = '<div id="exampleBreadcrumb" class="rup_breadcrumb"></div>';
-      $('body').append(html);
-      $breadcrumb = $('#exampleBreadcrumb');
-      $breadcrumb.rup_breadCrumb({
-        breadCrumb: {}
-      });
-    });
-    afterAll(() => {
-      $('body').html('');
-    });
-
-    it('El breadcrumb debe estar definido', () => {
-      expect($breadcrumb.find('span.rup-breadCrumbs_span:first').size()).toBe(1);
-    });
-
-
-  });
-  describe('Test de los métodos públicos', () => {
-    describe('Método destroy', () => {
-      beforeAll(() => {
-        rupObj('destroy');
-      });
-      it('No debe existir', () => {
-        expect(rupObj('destroy')).toThrowError();
-      });
-    });
-  });
+		beforeAll(() => {
+			html = '<div id="exampleBreadcrumb"></div>';
+			$('body').append(html);
+			$breadcrumb.rup_breadCrumb({
+				breadCrumb: {}
+			});
+			$breadcrumb = $('#exampleBreadcrumb');
+		});
+		// FIXME: $breadcrumb es undefined
+		it('El breadcrumb debe estar definido', () => {
+			expect($breadcrumb.find('span.rup-breadCrumbs_span:first').length).toBe(1);
+		});
+	});
+	describe('Test de los métodos públicos', () => {
+		describe('Método destroy', () => {
+			beforeAll(() => {
+					$breadcrumb.rup_breadcrumb('destroy');
+			});
+			it('No debe existir', () => {
+					expect(() => {$breadcrumb.rup_breadcrumb('destroy')}).toThrowError();
+			});
+	});
+	});
 });
