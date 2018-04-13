5d82bd22c559da8d9602747cf7ffe196d51389db
ecarrasson <ecarrasson@euro-help.es>
2018-04-13 08:57:21 +0200

    Añadido MButton

--- spec/button/rup.button.spec.js
+++ spec/button/rup.button.spec.js
@@ -8,17 +8,47 @@ describe('TEST Button', () => {
     describe('Creacion', () => {
         
         beforeAll(() => {
-            var html= '<button id="exampleButton"></button>';
+            var html= '<button id="exampleButton"></button>'
+                    +  '<div class="rup-mbutton">'
+                    +       '<button type="button" id="exampleMButton" data-mbutton="true">'
+                    +           '<i class="fa fa-cog" aria-hidden="true"></i> <span'
+                    +              'class="rup-ui-button-text hidden-md-down">MButton</span>'
+                    +       '</button>'
+                    +       '<ul id="mbuttonContainer" class="rup-mbutton-container"'
+                    +            'aria-labelledby="exampleMButton">'
+                    +            '<li>'
+                    +                '<button type="button" id="mbutton-buttonNew">'
+                    +                    '<i class="fa fa-cog" aria-hidden="true"></i> <span'
+                    +                        'class="rup-ui-button-text hidden-md-down">Nuevo</span>'
+                    +                '</button>'
+                    +            '</li>'
+                    +            '<li>'
+                    +                '<button type="button" id="mbutton-buttonEdit">'
+                    +                    '<i class="fa fa-cog" aria-hidden="true"></i> <span'
+                    +                        'class="rup-ui-button-text hidden-md-down">Editar</span>'
+                    +                '</button>'
+                    +            '</li>'
+                    +            '<li>'
+                    +                '<button type="button" id="mbutton-buttonCancel">'
+                    +                    '<i class="fa fa-cog" aria-hidden="true"></i> <span'
+                    +                        'class="rup-ui-button-text hidden-md-down">Cancelar</span>'
+                    +                '</button>'
+                    +            '</li>'
+                    +        '</ul>'
+                    +    '</div>';
             $('body').append(html);
-
-            $button = $('#exampleButton');
-            $button.rup_button({});
+            $('#exampleButton').rup_button({});
+            $('#exampleMButton').rup_button({});
+            $button  = $('#exampleButton');
+            $mButton = $('#exampleMButton');
+            
         });
     });
     describe('Métodos públicos', () => {
         describe('Método defaults', () => {
             it('El objeto debe estar definido', () => {
                 expect($button.rup_button('defaults')).toBeDefined();
+                expect($mButton.rup_button('defaults')).toBeDefined();
             });
         });
     });
