bc41f5b1acc4a5188c8f97b992f9cb77853f74a4
ecarrasson <ecarrasson@euro-help.es>
2018-04-13 10:09:03 +0200

    Solventados bugs

--- spec/lang/rup.lang.spec.js
+++ spec/lang/rup.lang.spec.js
@@ -1,10 +1,10 @@
 import 'jquery';
 import 'jasmine-jquery';
-import 'rup.accordion';
+import 'rup.lang';
 
 describe('Test Lang >', () => {
     var $lang;
-    describe('Creación >', () => {
+    beforeAll(() => {
         var html = '<a class="nav-link rup-nav-tool-icon" href="#" ' +
                     'id="exampleLang" data-toggle="dropdown">' +
                         '<i class="fa fa-globe" aria-hidden="true"></i>' +
@@ -13,7 +13,8 @@ describe('Test Lang >', () => {
         $('body').append(html);
         $('#exampleLang').rup_language({languages: jQuery.rup.AVAILABLE_LANGS_ARRAY});
         $lang = $('#exampleLang');
-
+    });
+    describe('Creación >', () => {
         it('Debe tener las clases correspondientes', () => {
             expect($lang).toHaveClass('ui-widget dropdown-toggle');
         });
