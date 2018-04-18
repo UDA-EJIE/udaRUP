eded1c9cbd67880a96967327cb4aaa7049648057
ecarrasson <ecarrasson@euro-help.es>
2018-04-18 13:49:52 +0200

    Test Wizard : Initial Commit Esquema

---
+++ spec/wizard/rup.wizard.spec.js
@@ -0,0 +1,22 @@
+import 'jquery';
+import 'jasmine-jquery';
+import 'rup.wizard';
+
+describe('Test Wizard > ', () => {
+    var $wizard;
+    beforeAll(() => {});
+    describe('Creación > ', () => {});
+    describe('Métodos públicos > ', () =>{
+        describe('Método step > '  , () => {});
+        describe('Método first > ' , () => {});
+        describe('Método last > '  , () => {});
+        describe('Método getCurrentStep > '         , () => {});
+        describe('Método isCurrentStep > '          , () => {});
+        describe('Método isCurrentStepFirst > '     , () => {});
+        describe('Método isCurrentStepLast > '      , () => {});
+        describe('Método isCurrentStepSummary > '   , () => {});
+        describe('Método enableStep > '     , () => {});
+        describe('Método disableStep > '    , () => {});
+        describe('Método isStepDisabled > ' , () => {});
+    });
+});
