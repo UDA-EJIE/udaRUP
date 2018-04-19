006833a42b5f0c245f18d2def799cd880cc3dcb8
ecarrasson <ecarrasson@euro-help.es>
2018-04-19 08:45:40 +0200

    Test Upload: Initial Commit: Esquema

---
+++ spec/upload/rup.upload.spec.js
@@ -0,0 +1,15 @@
+import 'jquery';
+import 'jasmine-jquery';
+import 'rup.upload';
+
+describe('Test Upload > ', () => {
+    var $upload;
+    beforeAll(() => {});
+    describe('Creación > ', () => {});
+    describe('Métodos públicos > ', () => {
+        describe('Método add > ', () => {});
+        describe('Método disable > ', () => {});
+        describe('Método enable > ', () => {});
+        describe('Método destroy > ', () => {});
+    });
+});
