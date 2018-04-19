5a0a1ff197ab340035db2f8a2a9592e5165929c4
ecarrasson <ecarrasson@euro-help.es>
2018-04-19 09:00:21 +0200

    Setup hecho: Preparado el entorno de pruebas

--- spec/upload/rup.upload.spec.js
+++ spec/upload/rup.upload.spec.js
@@ -4,7 +4,20 @@ import 'rup.upload';
 
 describe('Test Upload > ', () => {
     var $upload;
-    beforeAll(() => {});
+    beforeAll(() => {
+        let html =   '<span class="btn btn-success fileinput-button">'
+                +       '<i class="glyphicon glyphicon-plus"></i>'
+                +       '<span>Seleccionar...</span>'
+                +       '<input id="exampleUpload" type="file" name="files[]" data-url="../upload" multiple="multiple" />'
+                +    '</span>';
+        let props = {
+            dataType: 'json',
+            uploadTemplateId:false,
+            downloadTemplateId:false
+        };
+        $('#exampleUpload').rup_upload(props);
+        $upload = $('#exampleUpload');
+    });
     describe('Creación > ', () => {});
     describe('Métodos públicos > ', () => {
         describe('Método add > ', () => {});
