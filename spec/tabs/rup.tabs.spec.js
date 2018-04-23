9933aed64381bf66a98a41a4c912190ac5e89450
ecarrasson <ecarrasson@euro-help.es>
2018-04-23 17:24:14 +0200

    He conseguido hacer funcionar enable y disable tabs

--- spec/tabs/rup.tabs.spec.js
+++ spec/tabs/rup.tabs.spec.js
@@ -2,12 +2,13 @@ import 'jquery';
 import 'jasmine-jquery';
 import 'rup.tabs';
 
-describe('Test Tabs: ', () => {
+describe('Test Tabs > ', () => {
     var $tabs;
     beforeAll(() => {
         var html = '<div id="exampleTabs"></div>'
                  + '<div id="cont1" style="display:none;">Contenido 1</div>'
-                 + '<div id="cont2" style="display:none;">Contenido 2</div>';
+                 + '<div id="cont2" style="display:none;">Contenido 2</div>'
+                 + '<div id="cont3" style="display:none;">Contenido 3</div>';
         $('body').append(html);
         var opts = {
             tabs:[
@@ -18,28 +19,57 @@ describe('Test Tabs: ', () => {
         $('#exampleTabs').rup_tabs(opts);
         $tabs = $('#exampleTabs');
     });
-    describe('Creacion', () => () => {
+    describe('Creacion >', () => () => {
         it('El contenedor debe tener las classes apropiadas', () => {
             expect($tabs.hasClass('rup-tabs_container ui-tabs ui-corner-all ui-widget ui-widget-content')).toBeTruthy();
         });
     });
-    describe('Métodos Públicos', () => {
-        describe('Métodos enableTabs y disableTabs', () => {
-            //No funcionan en ejie.eus
+    describe('Métodos Públicos > ', () => {
+        describe('Métodos enableTabs y disableTabs > ', () => {
+            describe('Método disableTabs > ', () => {
+                beforeAll(() => {
+                    $tabs.rup_tabs('enableTabs',{
+                        idTab:'exampleTabs',
+                        position: 1
+                    });
+                    $tabs.rup_tabs('disableTabs',{
+                        idTab:'exampleTabs',
+                        position: 1
+                    });
+                });
+                it('Debe tener la clase que deshabilita la pestaña', () => {
+                    expect($('li[aria-labelledby="Tab2"').hasClass('ui-state-disabled')).toBeTruthy();
+                });
+            });
+            describe('Método enableTabs > ', () => {
+                beforeAll(() => {
+                    $tabs.rup_tabs('disableTabs',{
+                        idTab:'exampleTabs',
+                        position: 1
+                    });
+                    $tabs.rup_tabs('enableTabs',{
+                        idTab:'exampleTabs',
+                        position: 1
+                    });
+                });
+                it('No debe tener la clase que deshabilita la pestaña', () => {
+                    expect($('li[aria-labelledby="Tab2"').hasClass('ui-state-disabled')).toBeFalsy();
+                });
+            });
         });
-        describe('Método loadTab', () => {
+        describe('Método loadTab > ', () => {
             //Requiere recarga desde servidor
         });
-        describe('Método changeUrlTab', () => {
+        describe('Método changeUrlTab > ', () => {
             //Requiere llamadas xhr
         });
-        describe('Método changeLayerTab', () => {
+        describe('Método changeLayerTab > ', () => {
             //No funciona en ejie.eus
         });
-        describe('Método selectTab', () => {
+        describe('Método selectTab > ', () => {
             //No funciona en ejie.eus
         });
-        describe('Método addTab', () => {
+        describe('Método addTab > ', () => {
             //No funciona en ejie.eus
         });
     });
