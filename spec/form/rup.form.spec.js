9447b423b5ff9ba75de69133bb0c9d90caeebf8e
ecarrasson <ecarrasson@euro-help.es>
2018-04-30 12:39:44 +0200

    cambios menores

--- spec/form/rup.form.spec.js
+++ spec/form/rup.form.spec.js
@@ -1,14 +1,15 @@
 import 'jquery';
 import 'jasmine-jquery';
 import 'rup.form';
+import 'form2object';
 
 describe('Test Form', () => {
 	var $form;
 	beforeAll(() => {
-		var html = '<form id ="exampleForm">'
-              		+   '<input type="text" value="txt1" id="input1"></input>'
-              		+   '<input type="text" value="txt2" id="input2"></input>'
-              		+   '<select id="input3">'
+		var html = '<form name = "exampleForm" id ="exampleForm">'
+              		+   '<input type="text" value="txt1" name="input1" id="input1"></input>'
+              		+   '<input type="text" value="txt2" name="input2" id="input2"></input>'
+              		+   '<select name="input3" id="input3">'
               		+     '<option value="opt1">Opcion 1</input>'
               		+     '<option value="opt2">Opcion 2</input>'
               		+   '</select>'
@@ -64,19 +65,19 @@ describe('Test Form', () => {
 		describe('Método fieldSerialize >',() => {
 			it('Debe devolver un string con los datos de los fields', () => {
 				let out = 'input1=txt1&input2=txt2';
-				expect($form.rup_form('fieldSerialize')).toBe(out);
+				expect($('input', $form).rup_form('fieldSerialize')).toBe(out);
 			});
 		});
 		describe('Método fieldValue', () => {
 			it('Debe devolver los valores en un array', () => {
-				let out = ['txt1', 'txt2', 'opt1'];
-				expect($form.rup_form('fieldValue')).toBe(out);
+				let out = 'txt1,txt2';
+				expect($('input', $form).rup_form('fieldValue').join()).toBe(out);
 			});
 		});
 		describe('Método formToJson', () => {
 			it('Debe devolver los datos en un string en formato Json', () => {
 				let out = {input1:'txt1', input2:'txt2', input3:'opt1'};
-				expect(JSON.parse($form.rup_form('formToJson'))).toBe(out);
+				expect($form.rup_form('formToJson')).toBe(out);
 			});
 		});
 		// TODO: Conseguir el objeto de configuracion del form y probar esto.
@@ -85,8 +86,10 @@ describe('Test Form', () => {
 				$form.rup_form('formClear');
 			});
 			it('Deben quedar todos los inputs vacíos', () => {
-				expect($('#input1')).toBe('');
-				expect($('#input2')).toBe('');
+				setTimeout(() => {
+					expect($('#input1').val()).toBe('');
+					expect($('#input2').val()).toBe('');
+				}, 1500);
 			});
 		});
 
