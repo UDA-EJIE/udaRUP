ac4dd2154a3019063a4664927301e890ad36414d
ecarrasson <ecarrasson@euro-help.es>
2018-05-10 17:15:59 +0200

    Cambiado beforeAll -> beforeEach

--- spec/button/rup.button.spec.js
+++ spec/button/rup.button.spec.js
@@ -5,40 +5,43 @@ import 'rup.button';
 
 describe('TEST Button', () => {
     var $button;
-    beforeAll(() => {
-        var html= '<button id="exampleButton"></button>'
-                +  '<div class="rup-mbutton">'
-                +       '<button type="button" id="exampleMButton" data-mbutton="true">'
-                +           '<i class="fa fa-cog" aria-hidden="true"></i> <span'
-                +              'class="rup-ui-button-text hidden-md-down">MButton</span>'
-                +       '</button>'
-                +       '<ul id="mbuttonContainer" class="rup-mbutton-container"'
-                +            'aria-labelledby="exampleMButton">'
-                +            '<li>'
-                +                '<button type="button" id="mbutton-buttonNew">'
-                +                    '<i class="fa fa-cog" aria-hidden="true"></i> <span'
-                +                        'class="rup-ui-button-text hidden-md-down">Nuevo</span>'
-                +                '</button>'
-                +            '</li>'
-                +            '<li>'
-                +                '<button type="button" id="mbutton-buttonEdit">'
-                +                    '<i class="fa fa-cog" aria-hidden="true"></i> <span'
-                +                        'class="rup-ui-button-text hidden-md-down">Editar</span>'
-                +                '</button>'
-                +            '</li>'
-                +            '<li>'
-                +                '<button type="button" id="mbutton-buttonCancel">'
-                +                    '<i class="fa fa-cog" aria-hidden="true"></i> <span'
-                +                        'class="rup-ui-button-text hidden-md-down">Cancelar</span>'
-                +                '</button>'
-                +            '</li>'
-                +        '</ul>'
-                +    '</div>';
+    beforeEach(() => {
+        var html= '<button id="exampleButton"></button>\
+                  <div class="rup-mbutton">\
+                       <button type="button" id="exampleMButton" data-mbutton="true">\
+                           <i class="fa fa-cog" aria-hidden="true"></i> <span\
+                              class="rup-ui-button-text hidden-md-down">MButton</span>\
+                       </button>\
+                       <ul id="mbuttonContainer" class="rup-mbutton-container"\
+                            aria-labelledby="exampleMButton">\
+                            <li>\
+                                <button type="button" id="mbutton-buttonNew">\
+                                    <i class="fa fa-cog" aria-hidden="true"></i> <span\
+                                        class="rup-ui-button-text hidden-md-down">Nuevo</span>\
+                                </button>\
+                            </li>\
+                            <li>\
+                                <button type="button" id="mbutton-buttonEdit">\
+                                    <i class="fa fa-cog" aria-hidden="true"></i> <span\
+                                        class="rup-ui-button-text hidden-md-down">Editar</span>\
+                                </button>\
+                            </li>\
+                            <li>\
+                                <button type="button" id="mbutton-buttonCancel">\
+                                    <i class="fa fa-cog" aria-hidden="true"></i> <span\
+                                        class="rup-ui-button-text hidden-md-down">Cancelar</span>\
+                                </button>\
+                            </li>\
+                        </ul>\
+                    </div>';
         $('body').append(html);
         $('#exampleButton').rup_button({});
         $('#exampleMButton').rup_button({});
         $button  = $('#exampleButton');
         $mButton = $('#exampleMButton');
+    });
+    afterEach(() => {
+        $('body').html('');
     });
     describe('Creación >', () => {
         it('Deben ser rup-buttons',() => {
