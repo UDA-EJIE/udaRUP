15531b55e30448af066ed1b5f2077829f88cc93f
ecarrasson <ecarrasson@euro-help.es>
2018-05-24 16:07:45 +0200

    Test spinner reimplementado

--- spec/spinner/rup.spinner.spec.js
+++ spec/spinner/rup.spinner.spec.js
@@ -20,47 +20,13 @@ describe('Test Spinner > ', () => {
         });
     });
     describe('Métodos publicos > ', () => {
-        describe('Métodos setRupValue y getRupValue > ', () => {});
-        describe('Método destroy > ', () => {});
+        describe('Métodos setRupValue y getRupValue > ', () => {
+            beforeEach(() => {
+                $spinner.rup_spinner('setRupValue', 487);
+            });
+            it('Debe cambiar convenientemente el valor', () => {
+                expect($spinner.rup_spinner('getRupValue')).toBe(487);
+            });
+        });
     });
 });
-
-/*
-describe('Test Spinner: ', () => {
-    var $spinner;
-    describe('Creación: ', () => {
-        beforeAll(() => {
-            var html = '<input id="exampleSpinner"></input>';
-            $('body').append(html);
-            $('#exampleSpinner').rup_spinner();
-            $spinner = $('#exampleSpinner');
-        });
-
-        it('Debe tener la clase rup_spinner', () => {
-            expect($spinner.hasClass('rup_spinner ui-spinner-input')).toBeTruthy();
-        });
-    });
-    describe('Métodos públicos:', () => {
-      describe('Método getRupValue:', () => {
-          it('Devuelve un valor:', () => {
-              expect($spinner.rup_spinner('getRupValue')).toBeDefined();
-          });
-      });
-      describe('Método setRupValue', () => {
-          beforeAll(() => {
-              $spinner.rup_spinner('setRupValue', 50);
-          });
-          it('Debe actualizar el valor:', () => {
-              expect($spinner.rup_spinner('getRupValue')).toBe(50);
-          });
-      });
-      describe('Método destroy', () => {
-          beforeAll(() => {
-              $spinner.spinner('destroy');
-          });
-          it('No debe existir', () => {
-              expect(() => {$spinner.spinner('destroy');}).toThrowError();
-          });
-      });
-    });
-});*/
