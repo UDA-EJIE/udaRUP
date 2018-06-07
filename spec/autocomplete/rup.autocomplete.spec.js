0cd28bf98098a9e093dbe5f5124b1c77102be380
Lander Laparra <lander.laparra@gmail.com>
2018-06-07 10:54:25 +0200

    Correcciones en los test de autocomplete, falta mucho...

--- spec/autocomplete/rup.autocomplete.spec.js
+++ spec/autocomplete/rup.autocomplete.spec.js
@@ -1,3 +1,5 @@
+/* jslint esnext: true, multistr: true */
+
 import 'jquery';
 import 'jasmine-jquery';
 import 'rup.autocomplete';
@@ -8,30 +10,59 @@ describe('Test Autocomplete > ', () => {
         let html = '<input type="text" id="exampleAutocomplete">\
                     <input type="text" id="exampleAutocompleteDos">\
                     <input type="text" id="exampleAutocompleteTres">';
-		$('body').append(html);
-        let sourceJson = [
-            { i18nCaption:'ab', value:'ab_value' },
-            { i18nCaption:'tc', value:'tc_value' },
-            { i18nCaption:'ud', value:'ud_value' },
-            { i18nCaption:'le', value:'le_value' },
-            { i18nCaption:'af', value:'af_value' },
-            { i18nCaption:'mg', value:'mg_value' },
-            { i18nCaption:'ah', value:'ah_value' },
-            { i18nCaption:'ui', value:'ui_value' },
-            { i18nCaption:'uj', value:'uj_value' },
-            { i18nCaption:'ak', value:'ak_value' }
+        $('body').append(html);
+        let sourceJson = [{
+                i18nCaption: 'ab',
+                value: 'ab_value'
+            },
+            {
+                i18nCaption: 'tc',
+                value: 'tc_value'
+            },
+            {
+                i18nCaption: 'ud',
+                value: 'ud_value'
+            },
+            {
+                i18nCaption: 'le',
+                value: 'le_value'
+            },
+            {
+                i18nCaption: 'af',
+                value: 'af_value'
+            },
+            {
+                i18nCaption: 'mg',
+                value: 'mg_value'
+            },
+            {
+                i18nCaption: 'ah',
+                value: 'ah_value'
+            },
+            {
+                i18nCaption: 'ui',
+                value: 'ui_value'
+            },
+            {
+                i18nCaption: 'uj',
+                value: 'uj_value'
+            },
+            {
+                i18nCaption: 'ak',
+                value: 'ak_value'
+            }
         ];
-		$('#exampleAutocomplete').rup_autocomplete({
-			source: sourceJson,
-			defaultValue: 'a',
-			contains: false,
-			delay:0
+        $('#exampleAutocomplete').rup_autocomplete({
+            source: sourceJson,
+            defaultValue: 'a',
+            contains: false,
+            delay: 0
         });
         $('#exampleAutocompleteDos').rup_autocomplete({
-			source: sourceJson,
-			defaultValue: 'a',
-			contains: true,
-			delay:0
+            source: sourceJson,
+            defaultValue: 'a',
+            contains: true,
+            delay: 0
         });
         $('#exampleAutocompleteTres').rup_autocomplete({
             source: 'api/autocomplete/remote',
@@ -41,88 +72,86 @@ describe('Test Autocomplete > ', () => {
             },
             minLength: 4
         });
-		$autocomplete = $('#exampleAutocomplete');
+        $autocomplete = $('#exampleAutocomplete');
         $autocomplete2 = $('#exampleAutocompleteDos');
         $autocomplete3 = $('#exampleAutocompleteTres');
-		$autocompleteLabel = $('#exampleAutocomplete_label');
-		$autocompleteLabel2 = $('#exampleAutocompleteDos_label');
-		$autocompleteLabel3 = $('#exampleAutocompleteTres_label');
+        $autocompleteLabel = $('#exampleAutocomplete_label');
+        $autocompleteLabel2 = $('#exampleAutocompleteDos_label');
+        $autocompleteLabel3 = $('#exampleAutocompleteTres_label');
     });
     afterEach(() => {
-      	$('body').html('');
+        $('body').html('');
     });
     describe('Creación > ', () => {
-		it('El elemento html debe presentar cambios', () => {
-			setTimeout(() => {
-				expect($autocomplete.attr('ruptype')).toBe('autocomplete');
-                expect($autocomplete.hasClass('rup-autocomplete_label ui-autocomplete-input')).toBeTruthy();
-                expect($autocomplete2.attr('ruptype')).toBe('autocomplete');
-				expect($autocomplete2.hasClass('rup-autocomplete_label ui-autocomplete-input')).toBeTruthy();
-				expect($autocomplete3.attr('ruptype')).toBe('autocomplete');
-				expect($autocomplete3.hasClass('rup-autocomplete_label ui-autocomplete-input')).toBeTruthy();
-			}, 1500);
-		});
+        it('El elemento html debe presentar cambios', () => {
+            expect($autocomplete.attr('ruptype')).toBe('autocomplete');
+            expect($autocomplete.prev().hasClass('rup-autocomplete_label ui-autocomplete-input')).toBeTruthy();
+            expect($autocomplete2.attr('ruptype')).toBe('autocomplete');
+            expect($autocomplete2.prev().hasClass('rup-autocomplete_label ui-autocomplete-input')).toBeTruthy();
+            expect($autocomplete3.attr('ruptype')).toBe('autocomplete');
+            expect($autocomplete3.prev().hasClass('rup-autocomplete_label ui-autocomplete-input')).toBeTruthy();
+        });
     });
     describe('Métodos públicos > ', () => {
         describe('Método on > ', () => {
             beforeEach(() => {
-				setTimeout(() => {
-					$autocompleteLabel.rup_autocomplete('close');
-					$autocompleteLabel.rup_autocomplete('off');
-					$autocompleteLabel.rup_autocomplete('on');
-                    $autocompleteLabel.rup_autocomplete('search','u');
+                setTimeout(() => {
+                    $autocompleteLabel.rup_autocomplete('close');
+                    $autocompleteLabel.rup_autocomplete('off');
+                    $autocompleteLabel.rup_autocomplete('on');
+                    $autocompleteLabel.rup_autocomplete('search', 'u');
                     $autocompleteLabel2.rup_autocomplete('close');
-					$autocompleteLabel2.rup_autocomplete('off');
-					$autocompleteLabel2.rup_autocomplete('on');
-					$autocompleteLabel2.rup_autocomplete('search', 'u');
-					$autocompleteLabel3.rup_autocomplete('close');
-					$autocompleteLabel3.rup_autocomplete('off');
-					$autocompleteLabel3.rup_autocomplete('on');
-					$autocompleteLabel3.rup_autocomplete('search', 'u');
-				}, 1500);
+                    $autocompleteLabel2.rup_autocomplete('off');
+                    $autocompleteLabel2.rup_autocomplete('on');
+                    $autocompleteLabel2.rup_autocomplete('search', 'u');
+                    $autocompleteLabel3.rup_autocomplete('close');
+                    $autocompleteLabel3.rup_autocomplete('off');
+                    $autocompleteLabel3.rup_autocomplete('on');
+                    $autocompleteLabel3.rup_autocomplete('search', 'u');
+                }, 1500);
             });
             it('Debe mostrarse el menu', () => {
-				setTimeout(() => {
-					expect($('#exampleAutocomplete_menu').is(':visible')).toBeTruthy();
-					expect($('#exampleAutocompleteDos_menu').is(':visible')).toBeTruthy();
-					expect($('#exampleAutocompleteTres_menu').is(':visible')).toBeTruthy();
-				}, 1500);
+                setTimeout(() => {
+                    expect($('#exampleAutocomplete_menu').is(':visible')).toBeTruthy();
+                    expect($('#exampleAutocompleteDos_menu').is(':visible')).toBeTruthy();
+                    expect($('#exampleAutocompleteTres_menu').is(':visible')).toBeTruthy();
+                }, 1500);
             });
         });
         describe('Método off > ', () => {
             beforeEach(() => {
-				setTimeout(() => {
-					$autocompleteLabel.rup_autocomplete('close');
-					$autocompleteLabel.rup_autocomplete('on');
-					$autocompleteLabel.rup_autocomplete('off');
-                    $autocompleteLabel.rup_autocomplete('search','u');
+                setTimeout(() => {
+                    $autocompleteLabel.rup_autocomplete('close');
+                    $autocompleteLabel.rup_autocomplete('on');
+                    $autocompleteLabel.rup_autocomplete('off');
+                    $autocompleteLabel.rup_autocomplete('search', 'u');
                     $autocompleteLabel2.rup_autocomplete('close');
-					$autocompleteLabel2.rup_autocomplete('on');
-					$autocompleteLabel2.rup_autocomplete('off');
-					$autocompleteLabel2.rup_autocomplete('search','u');
-				}, 1500);
+                    $autocompleteLabel2.rup_autocomplete('on');
+                    $autocompleteLabel2.rup_autocomplete('off');
+                    $autocompleteLabel2.rup_autocomplete('search', 'u');
+                }, 1500);
             });
             it('Debe mostrarse el menu', () => {
                 setTimeout(() => {
                     expect($('#exampleAutocomplete_menu').is(':visible')).toBeFalsy();
-				    expect($('#exampleAutocompleteDos_menu').is(':visible')).toBeFalsy();
+                    expect($('#exampleAutocompleteDos_menu').is(':visible')).toBeFalsy();
                 }, 1500);
             });
         });
         describe('Método option > ', () => {
             beforeEach(() => {
-				setTimeout(() => {
-					$autocompleteLabel.rup_autocomplete('close');
+                setTimeout(() => {
+                    $autocompleteLabel.rup_autocomplete('close');
                     $autocompleteLabel.rup_autocomplete('option', 'combobox', true);
                     $autocompleteLabel2.rup_autocomplete('close');
-                	$autocompleteLabel2.rup_autocomplete('option', 'combobox', true);
-				}, 1500);
+                    $autocompleteLabel2.rup_autocomplete('option', 'combobox', true);
+                }, 1500);
             });
             it('Debe tener la clase de combobox', () => {
-				setTimeout(() => {
+                setTimeout(() => {
                     expect($autocompleteLabel.hasClass('rup-combobox-input')).toBeTruthy();
                     expect($autocompleteLabel2.hasClass('rup-combobox-input')).toBeTruthy();
-				}, 1500);
+                }, 1500);
             });
         });
         describe('Método search > ', () => {
@@ -161,99 +190,99 @@ describe('Test Autocomplete > ', () => {
         });
         describe('Método close > ', () => {
             beforeEach(() => {
-				setTimeout(() => {
-					$autocompleteLabel.rup_autocomplete('close');
-				}, 1500);
+                setTimeout(() => {
+                    $autocompleteLabel.rup_autocomplete('close');
+                }, 1500);
             });
             it('Deben mostrarse la opciones', () => {
-				setTimeout(() => {
-					expect($('#autocomplete_menu').is(':visible')).toBeFalsy();
-				}, 1500);
+                setTimeout(() => {
+                    expect($('#autocomplete_menu').is(':visible')).toBeFalsy();
+                }, 1500);
             });
         });
         describe('Método val > ', () => {
-            beforeEach(()=> {
-				setTimeout(() => {
-					$autocompleteLabel.rup_autocomplete('close');
-                	$autocompleteLabel.rup_autocomplete('search', 'ui');
-				}, 1500);
+            beforeEach(() => {
+                setTimeout(() => {
+                    $autocompleteLabel.rup_autocomplete('close');
+                    $autocompleteLabel.rup_autocomplete('search', 'ui');
+                }, 1500);
             });
             it('Debe devolver el valor seleccionado', () => {
-				setTimeout(() => {
-					expect($autocompleteLabel.rup_autocomplete('val')).toBe('ui_value');
-				}, 1500);
+                setTimeout(() => {
+                    expect($autocompleteLabel.rup_autocomplete('val')).toBe('ui_value');
+                }, 1500);
             });
         });
         describe('Método set > ', () => {
-            beforeEach(()=> {
-				setTimeout(() => {
-					$autocompleteLabel.rup_autocomplete('close');
-                	$autocompleteLabel.rup_autocomplete('set', 'ui', 'ui_value');
-				}, 1500);
+            beforeEach(() => {
+                setTimeout(() => {
+                    $autocompleteLabel.rup_autocomplete('close');
+                    $autocompleteLabel.rup_autocomplete('set', 'ui', 'ui_value');
+                }, 1500);
             });
             it('Debe devolver el valor seleccionado', () => {
-				setTimeout(() => {
-					expect($autocompleteLabel.rup_autocomplete('val')).toBe('ui_value');
-				}, 1500);
+                setTimeout(() => {
+                    expect($autocompleteLabel.rup_autocomplete('val')).toBe('ui_value');
+                }, 1500);
             });
         });
         describe('Método getRupValue > ', () => {
-            beforeEach(()=> {
-				setTimeout(() => {
-					$autocompleteLabel.rup_autocomplete('close');
-                	$autocompleteLabel.rup_autocomplete('set', 'ui', 'ui_value');
-				}, 1500);
+            beforeEach(() => {
+                setTimeout(() => {
+                    $autocompleteLabel.rup_autocomplete('close');
+                    $autocompleteLabel.rup_autocomplete('set', 'ui', 'ui_value');
+                }, 1500);
             });
             it('Debe devolver el valor seleccionado', () => {
-				setTimeout(() => {
-					expect($autocompleteLabel.rup_autocomplete('getRupValue')).toBe('ui_value');
-				}, 1500);
+                setTimeout(() => {
+                    expect($autocompleteLabel.rup_autocomplete('getRupValue')).toBe('ui_value');
+                }, 1500);
             });
         });
         describe('Método setRupValue > ', () => {
-            beforeEach(()=> {
-				setTimeout(() => {
-					$autocompleteLabel.rup_autocomplete('close');
-                	$autocompleteLabel.rup_autocomplete('setRupValue', 'ui_value');
-				}, 1500);
+            beforeEach(() => {
+                setTimeout(() => {
+                    $autocompleteLabel.rup_autocomplete('close');
+                    $autocompleteLabel.rup_autocomplete('setRupValue', 'ui_value');
+                }, 1500);
             });
             it('Debe devolver el valor seleccionado', () => {
-				setTimeout(() => {
-					expect($autocompleteLabel.rup_autocomplete('getRupValue')).toBe('ui_value');
-				}, 1500);
+                setTimeout(() => {
+                    expect($autocompleteLabel.rup_autocomplete('getRupValue')).toBe('ui_value');
+                }, 1500);
             });
         });
         describe('Método disable > ', () => {
             beforeEach(() => {
-				setTimeout(() => {
-					$autocompleteLabel.rup_autocomplete('disable');
-				}, 1500);
+                setTimeout(() => {
+                    $autocompleteLabel.rup_autocomplete('disable');
+                }, 1500);
             });
             it('Debe tener el atributo de deshabilitado', () => {
-				setTimeout(() => {
-					expect($autocompleteLabel.attr('disabled')).toBe('disabled');
-				}, 1500);
+                setTimeout(() => {
+                    expect($autocompleteLabel.attr('disabled')).toBe('disabled');
+                }, 1500);
             });
         });
         describe('Método enable > ', () => {
             beforeEach(() => {
-				setTimeout(() => {
-					$autocompleteLabel.rup_autocomplete('disable');
-                	$autocompleteLabel.rup_autocomplete('enable');
-				}, 1500);
+                setTimeout(() => {
+                    $autocompleteLabel.rup_autocomplete('disable');
+                    $autocompleteLabel.rup_autocomplete('enable');
+                }, 1500);
             });
             it('Debe tener el atributo de deshabilitado', () => {
-				setTimeout(() => {
-					expect($autocompleteLabel.attr('disabled')).toBeUndefined();
-				}, 1500);
+                setTimeout(() => {
+                    expect($autocompleteLabel.attr('disabled')).toBeUndefined();
+                }, 1500);
             });
         });
         describe('Método destroy > ', () => {
             beforeEach(() => {
-				setTimeout(() => {
-					$autocomplete.rup_autocomplete('destroy');
-					$autocomplete2.rup_autocomplete('destroy');
-				}, 1500);
+                setTimeout(() => {
+                    $autocomplete.rup_autocomplete('destroy');
+                    $autocomplete2.rup_autocomplete('destroy');
+                }, 1500);
             });
             it('Intentar volver a destruir el objeto debe dar error', () => {
                 setTimeout(() => {
