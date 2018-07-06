18efae4f146edfae295dc4ee25caaf5bfc81247f
Xabi <xabierportu@gmail.com>
2018-07-06 12:22:59 +0200

    Añadida otra propiedad restante a Karma
    Se añade la propiedad de generar el archivo sin necesidad de crear una carpeta padre donde albergarlo.

--- karma.conf.js
+++ karma.conf.js
@@ -39,7 +39,7 @@ module.exports = function (config) {
           outputDir: 'spec', // where to put the reports 
           templatePath: null, // set if you moved jasmine_template.html
           focusOnFailures: false, // reports show failures on start
-          namedFiles: false, // name files instead of creating sub-directories
+          namedFiles: true, // name files instead of creating sub-directories
           pageTitle: 'Karma Report 3.5.0', // page title for reports; browser info by default
           urlFriendlyName: false, // simply replaces spaces with _ for files/dirs
           reportName: 'karma_report_3.5.0', // report summary filename; browser info by default
