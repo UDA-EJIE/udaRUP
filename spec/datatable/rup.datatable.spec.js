1f4f943ebbbc523f01db4c91bd39cb77bdc97c77
Lander Laparra <lander.laparra@gmail.com>
2018-09-03 13:54:11 +0200

    Correcciones de dependencias para el test

--- spec/datatable/rup.datatable.spec.js
+++ spec/datatable/rup.datatable.spec.js
@@ -1,6 +1,9 @@
 import 'jquery';
 import 'jasmine-jquery';
+import 'rup.dialog';
+import 'rup.table';
 import 'datatable/rup.datatable';
+import * as testutils from '../common/specCommonUtils.js';
 import * as consts from './datatable.html';
 
 var plugins = [{
@@ -13,96 +16,108 @@ var plugins = [{
 
 function testDatatable(plugin) {
     describe('Test DataTable ' + plugin.name + ' > ', () => {
+
+        beforeAll((done) => {
+            testutils.loadCss(done);
+        });
+
         var $datatable;
         beforeEach(() => {
             let opts = {
                 "fixedHeader": {
-                  "footer": false,
-                  "header": true
+                    "footer": false,
+                    "header": true
                 },
                 "filter": {
-                  "id": "example_filter_form",
-                  "filterToolbar": "example_filter_toolbar",
-                  "collapsableLayerId": "example_filter_fieldset"
+                    "id": "example_filter_form",
+                    "filterToolbar": "example_filter_toolbar",
+                    "collapsableLayerId": "example_filter_fieldset"
                 },
                 "multiSelect": {
-                  "style": "multi"
+                    "style": "multi"
                 },
                 "formEdit": {
-                  "detailForm": "#example_detail_div",
-                  "validate": {
-                    "rules": {
-                      "nombre": {
-                        "required": true
-                      },
-                      "apellidos": {
-                        "required": true
-                      },
-                      "edad": {
-                        "required": true
-                      }
-                    }
-                  },
-                  "titleForm": "Modificar registro"
+                    "detailForm": "#example_detail_div",
+                    "validate": {
+                        "rules": {
+                            "nombre": {
+                                "required": true
+                            },
+                            "apellidos": {
+                                "required": true
+                            },
+                            "edad": {
+                                "required": true
+                            }
+                        }
+                    },
+                    "titleForm": "Modificar registro"
                 },
                 "buttons": {
-                  "activate": true
+                    "activate": true
                 },
                 "seeker": {
-                  "colModel": [
-                    {
-                      "name": "id",
-                      "index": "id",
-                      "editable": true,
-                      "width": 80,
-                      "formoptions": {
-                        "rowpos": 1,
-                        "colpos": 1
-                      }
-                    },
-                    {
-                      "name": "nombre",
-                      "index": "nombre",
-                      "editable": true,
-                      "formoptions": {
-                        "rowpos": 2,
-                        "colpos": 1
-                      }
-                    },
-                    {
-                      "name": "apellidos",
-                      "index": "apellidos",
-                      "editable": true,
-                      "formoptions": {
-                        "rowpos": 3,
-                        "colpos": 1
-                      },
-                      "classes": "ui-ellipsis"
-                    },
-                    {
-                      "name": "edad",
-                      "index": "edad",
-                      "editable": true,
-                      "formoptions": {
-                        "rowpos": 4,
-                        "colpos": 1
-                      }
-                    }
-                ]
+                    "colModel": [{
+                            "name": "id",
+                            "index": "id",
+                            "editable": true,
+                            "width": 80,
+                            "formoptions": {
+                                "rowpos": 1,
+                                "colpos": 1
+                            }
+                        },
+                        {
+                            "name": "nombre",
+                            "index": "nombre",
+                            "editable": true,
+                            "formoptions": {
+                                "rowpos": 2,
+                                "colpos": 1
+                            }
+                        },
+                        {
+                            "name": "apellidos",
+                            "index": "apellidos",
+                            "editable": true,
+                            "formoptions": {
+                                "rowpos": 3,
+                                "colpos": 1
+                            },
+                            "classes": "ui-ellipsis"
+                        },
+                        {
+                            "name": "edad",
+                            "index": "edad",
+                            "editable": true,
+                            "formoptions": {
+                                "rowpos": 4,
+                                "colpos": 1
+                            }
+                        }
+                    ]
                 }
-              };
+            };
+
+            if ($('#content').length == 0) {
+                $('body').append('<div id="content"></div>');
+            }
+
+            debugger;
             $('#content').append(consts.html);
             $('#example').rup_datatable(opts);
             $datatable = $('#example');
         });
         describe('Creacion > ', () => {
             beforeEach((done) => {
-                $datatable.on('load', () => { done(); });
+                $datatable.on('load', () => {
+                    done();
+                });
             });
-            /*it('asd', () => {
+            it('asd', () => {
                 debugger;
                 expect('asd').toBe('asd');
-            });*/
+            });
         });
         describe('Pruebas plugins > ', () => {});
     });
