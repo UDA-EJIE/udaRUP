d58cb0fa0abc95260a4bc3cda9e575f24f96d36c
Lander Laparra <lander.laparra@gmail.com>
2018-10-02 10:02:33 +0200

    Se corrige el lanzador de pruebas para que se ejecuten todas

--- test.webpack.js
+++ test.webpack.js
@@ -1,6 +1,6 @@
-var testsContext = require.context('./spec/datatable', true, /spec\.js$/);
+var testsContext = require.context('./spec/', true, /spec\.js$/);
 testsContext.keys().forEach(testsContext);
 
 // var srcContext = require.context('./spec', true, /^((?!__tests__).)*.js$/);
-var srcContext = require.context('./spec/datatable', true, /spec\.js$/);
+var srcContext = require.context('./spec/', true, /spec\.js$/);
 srcContext.keys().forEach(srcContext);
