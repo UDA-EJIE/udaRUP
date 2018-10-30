d2462919c0d0c96f75b5fd531f1fc4b5d6a5c401
Xabi <xabierportu@gmail.com>
2018-10-30 16:48:27 +0100

    Corrección de id de navegación en editForm
    Se ha corregido un identificador que no estaba siendo generado de manera dinámica y hacia fallar el editForm.
    
    Co-authored-by: hmartinez85 <34453480+hmartinez85@users.noreply.github.com>

--- src/datatable/rup.table.editForm.js
+++ src/datatable/rup.table.editForm.js
@@ -671,7 +671,7 @@ function _updateDetailPagination(ctx,currentRowNum,totalRowNum){
 		$('#forward_' + tableId + ', #last_' + tableId, ctx.oInit.formEdit.detailForm).removeClass('ui-state-disabled');
 	}
 
-	$('#rup_table_selectedElements_table','#'+tableId).text(jQuery.jgrid.format(jQuery.rup.i18nParse(jQuery.rup.i18n.base, 'rup_datatable.defaults.detailForm_pager'), currentRowNum, totalRowNum));
+	$('#rup_table_selectedElements_' + tableId).text(jQuery.jgrid.format(jQuery.rup.i18nParse(jQuery.rup.i18n.base, 'rup_datatable.defaults.detailForm_pager'), currentRowNum, totalRowNum));
 }
 
 /**
