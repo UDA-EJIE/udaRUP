8c50c2239398b796645b0907e5f7eb00accba7fb
ecarrasson <ecarrasson@euro-help.es>
2018-11-23 13:24:21 +0100

    modificado audit.js

--- demo/routes/audit.js
+++ demo/routes/audit.js
@@ -2,4 +2,5 @@ exports.audit = function(req, res){
     console.info('=============================');
     console.info(JSON.stringify(req.body));
     console.info('=============================');
+    res.status(200).json(req.body);
 };
