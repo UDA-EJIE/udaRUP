4bd93ecda53a604defd19826753cac5c6bca45ba
Xabi <xabierportu@gmail.com>
2019-05-03 12:39:03 +0200

    Añadidas nuevas tareas de gulp
    Se han añadido tareas nuevas para automatizar los traspasos de las pruebas de jasmine.

---
+++ spec/rup.config.js
@@ -0,0 +1,19 @@
+
+
+	var	APP_RESOURCES = window.location.pathname.substring(1,window.location.pathname.indexOf('/3x'))+'/3x/x21a',
+	CTX_PATH = '/x21aResponsive/',
+	STATICS = '',
+    RESOURCES_PATH = '../../i18n/',
+	RUP = '../../rup',
+	WAR_NAME = "x21aApp",
+	//model
+	LAYOUT = "horizontal",
+	//mvc-config.xml
+	LOCALE_COOKIE_NAME = "language",
+	LOCALE_PARAM_NAME = "locale",
+	AVAILABLE_LANGS = "es,eu,en,fr",
+    DEFAULT_LANG = "es",
+	//breadCrumbs
+	LOGGED_USER = "NULL",
+	DESTROY_XLNETS_SESSION = "false";
+	document.cookie = "language=es";;
