201ba1c98a79429ef4c8361a4a9435e43a88bb53
Xabi <xabierportu@gmail.com>
2019-05-23 08:36:59 +0200

    Se sigue eliminando font-awesome
    Eliminadas más referencias a font-awesome.

--- spec/specCommonUtils.js
+++ spec/specCommonUtils.js
@@ -17,33 +17,29 @@ export function loadCss(callback) {
     return $.when($.ajax(DIST + '/css/externals/tether/tether.min.css'))
         .then((data, textStatus, jqXHR) => {
             $('head > style').append(data);
-            $.when($.ajax(DIST + '/css/externals/bootstrap/bt4.min.css'))
+            $.when($.ajax(DIST + '/css/externals/bootstrap/bootstrap.min.css'))
                 .then((data, textStatus, jqXHR) => {
                     $('head > style').append(data);
-                    $.when($.ajax(DIST + '/css/externals/font-awesome/font-awesome.min.css'))
-                        .then((data, textStatus, jqXHR) => {
-                            $('head > style').append(data);
-                            $.when($.ajax(DIST + '/css/rup-base.css'))
-                                .then((data, textStatus, jqXHR) => {
-                                    $('head > style').append(data);
-                                    $.when($.ajax(DIST + '/css/rup-theme.css'))
-                                        .then((data, textStatus, jqXHR) => {
-                                            $('head > style').append(data);
-                                            $.when($.ajax(DIST + '/css/rup-jqueryui-theme.css'))
+                        $.when($.ajax(DIST + '/css/rup-base.css'))
+                            .then((data, textStatus, jqXHR) => {
+                                $('head > style').append(data);
+                                $.when($.ajax(DIST + '/css/rup-theme.css'))
+                                    .then((data, textStatus, jqXHR) => {
+                                        $('head > style').append(data);
+                                        $.when($.ajax(DIST + '/css/rup-jqueryui-theme.css'))
+                                            .then((data, textStatus, jqXHR) => {
+                                                $('head > style').append(data);
+                                                $.when($.ajax(DIST + '/css/main.css'))
                                                 .then((data, textStatus, jqXHR) => {
                                                     $('head > style').append(data);
-                                                    $.when($.ajax(DIST + '/css/main.css'))
-                                                    .then((data, textStatus, jqXHR) => {
-                                                        $('head > style').append(data);
-                                                        if($('#content').length === 0) {
-                                                            $('body').append('<div id="content" class="container"></div>');
-                                                        }
-                                                        callback();
-                                                    });
+                                                    if($('#content').length === 0) {
+                                                        $('body').append('<div id="content" class="container"></div>');
+                                                    }
+                                                    callback();
                                                 });
-                                        });
-                                });
-                        });
+                                            });
+                                    });
+                            });
                 });
         });
 }
