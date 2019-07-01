7d451a584283e53ddd04f35c8266c6e166bda7c6
ecarrasson <ecarrasson@euro-help.es>
2019-07-01 14:12:14 +0200

    Solucionados errores de tests

--- test.webpack.js
+++ test.webpack.js
@@ -1,9 +1,9 @@
-//var testsContext = require.context('./spec', true, /(?<!X21a.*)\.spec\.js$/);
+var testsContext = require.context('./spec', true, /(?<!X21a.*)\.spec\.js$/);
 //var testsContext = require.context('./spec', true, /(breadcrumb)(?<!X21a.*)\.spec\.js$/);
-var testsContext = require.context('./spec', true, /(accordion|audit|autocomplete|breadcrumb|button|calendar|chart|combo|contextMenu|date|dialog|feedback|form|message|navbar|progressbar|report|slider|spinner|tabs|time|toolbar|tooltip|tree|upload|utils|validate|wizard)(?<!X21a.*)\.spec\.js$/);
+// var testsContext = require.context('./spec', true, /(accordion|audit|autocomplete|breadcrumb|button|calendar|chart|combo|contextMenu|date|dialog|feedback|form|message|navbar|progressbar|report|slider|spinner|tabs|time|toolbar|tooltip|tree|upload|utils|validate|wizard)(?<!X21a.*)\.spec\.js$/);
 testsContext.keys().forEach(testsContext);
 
-//var srcContext = require.context('./spec', true, /(?<!X21a.*)\.spec\.js$/);
+var srcContext = require.context('./spec', true, /(?<!X21a.*)\.spec\.js$/);
 //var srcContext = require.context('./spec', true, /(breadcrumb)(?<!X21a.*)\.spec\.js$/);
-var srcContext = require.context('./spec', true, /(accordion|audit|autocomplete|breadcrumb|button|calendar|chart|combo|contextMenu|date|dialog|feedback|form|message|navbar|progressbar|report|slider|spinner|tabs|time|toolbar|tooltip|tree|upload|utils|validate|wizard)(?<!X21a.*)\.spec\.js$/);
+// var srcContext = require.context('./spec', true, /(accordion|audit|autocomplete|breadcrumb|button|calendar|chart|combo|contextMenu|date|dialog|feedback|form|message|navbar|progressbar|report|slider|spinner|tabs|time|toolbar|tooltip|tree|upload|utils|validate|wizard)(?<!X21a.*)\.spec\.js$/);
 srcContext.keys().forEach(srcContext);
