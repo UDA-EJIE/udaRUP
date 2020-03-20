2eca10c2e12b8ffc4bf279af5e406da0df270ecf
Lander Laparra <llapara@eurohelp.es>
2020-03-20 15:10:15 +0100

    Linters en tabla y añadir el nuevo feedback a las validaciones del filtro

--- src/rup_table/rup.table.js
+++ src/rup_table/rup.table.js
@@ -818,10 +818,10 @@
                 
                 filterOpts.$filterButton = filterOpts.$filterContainer.find('#' + tableId + '_filter_filterButton');
                 filterOpts.$filterButton.on('click', function () {
-                	let customFiltrar = options.validarFiltrar;
-                	if($.isFunction(customFiltrar) && customFiltrar(options)){
-                		return false;
-                	}
+                    let customFiltrar = options.validarFiltrar;
+                    if ($.isFunction(customFiltrar) && customFiltrar(options)) {
+                        return false;
+                    }
                     $self._doFilter(options);
                 });
                 filterOpts.$clearButton = filterOpts.$filterContainer.find('#' + tableId + '_filter_cleanButton');
@@ -855,10 +855,10 @@
                 // Se asigna a la tecla ENTER la funcion de busqueda.
                 filterOpts.$filterContainer.bind('keydown', function (evt) {
                     if (evt.keyCode === 13) {
-                    	let customFiltrar = options.validarFiltrar;
-                    	if($.isFunction(customFiltrar) && customFiltrar(options)){
-                    		return false;
-                    	}
+                        let customFiltrar = options.validarFiltrar;
+                        if ($.isFunction(customFiltrar) && customFiltrar(options)) {
+                            return false;
+                        }
                         $self._doFilter(options);
                     }
                 });
@@ -898,6 +898,7 @@
                 // Validaciones 
                 if (filterOpts.rules) {
                     filterOpts.$filterContainer.rup_validate({
+                        feedback: options.feedback.$feedbackContainer,
                         rules: filterOpts.rules
                     });
                 }
