6aaaf42bfd53ddceb48bbd729d44230ebae71f89
Xabi <xaabi6@users.noreply.github.com>
2022-10-27 13:11:37 +0200

    Corregir obtención de i18n en el menú contextual
    Los recursos idiomáticos de las opciones que ofrece el menú no se estaban estableciendo correctamente.

--- src/rup.tree.js
+++ src/rup.tree.js
@@ -1930,14 +1930,113 @@
 			'contextmenu': {
 				'select_node': true,
 				'show_at_node': true,
-				'items': {
-					'create.label': $.rup.i18nParse($.rup.i18n.base, 'rup_tree.create'),
-					'rename.label': $.rup.i18nParse($.rup.i18n.base, 'rup_tree.rename'),
-					'remove.label': $.rup.i18nParse($.rup.i18n.base, 'rup_tree.remove'),
-					'ccp.label': $.rup.i18nParse($.rup.i18n.base, 'rup_tree.ccp.label'),
-					'ccp.submenu.cut.label': $.rup.i18nParse($.rup.i18n.base, 'rup_tree.ccp.cut'),
-					'ccp.submenu.copy.label': $.rup.i18nParse($.rup.i18n.base, 'rup_tree.ccp.copy'),
-					'ccp.submenu.paste.label': $.rup.i18nParse($.rup.i18n.base, 'rup_tree.ccp.paste'),
+				'items': function (o, cb) { // Could be an object directly
+					return {
+						"create" : {
+							"separator_before"	: false,
+							"separator_after"	: true,
+							"_disabled"			: false, //(this.check("create_node", data.reference, {}, "last")),
+							"label"				: $.rup.i18nParse($.rup.i18n.base, 'rup_tree.create'),
+							"action"			: function (data) {
+								var inst = $.jstree.reference(data.reference),
+									obj = inst.get_node(data.reference);
+								inst.create_node(obj, {}, "last", function (new_node) {
+									try {
+										inst.edit(new_node);
+									} catch (ex) {
+										setTimeout(function () { inst.edit(new_node); },0);
+									}
+								});
+							}
+						},
+						"rename" : {
+							"separator_before"	: false,
+							"separator_after"	: false,
+							"_disabled"			: false, //(this.check("rename_node", data.reference, this.get_parent(data.reference), "")),
+							"label"				: $.rup.i18nParse($.rup.i18n.base, 'rup_tree.rename'),
+							/*!
+							"shortcut"			: 113,
+							"shortcut_label"	: 'F2',
+							"icon"				: "glyphicon glyphicon-leaf",
+							*/
+							"action"			: function (data) {
+								var inst = $.jstree.reference(data.reference),
+									obj = inst.get_node(data.reference);
+								inst.edit(obj);
+							}
+						},
+						"remove" : {
+							"separator_before"	: false,
+							"icon"				: false,
+							"separator_after"	: false,
+							"_disabled"			: false, //(this.check("delete_node", data.reference, this.get_parent(data.reference), "")),
+							"label"				: $.rup.i18nParse($.rup.i18n.base, 'rup_tree.remove'),
+							"action"			: function (data) {
+								var inst = $.jstree.reference(data.reference),
+									obj = inst.get_node(data.reference);
+								if(inst.is_selected(obj)) {
+									inst.delete_node(inst.get_selected());
+								}
+								else {
+									inst.delete_node(obj);
+								}
+							}
+						},
+						"ccp" : {
+							"separator_before"	: true,
+							"icon"				: false,
+							"separator_after"	: false,
+							"label"				: $.rup.i18nParse($.rup.i18n.base, 'rup_tree.ccp.label'),
+							"action"			: false,
+							"submenu" : {
+								"cut" : {
+									"separator_before"	: false,
+									"separator_after"	: false,
+									"label"				: $.rup.i18nParse($.rup.i18n.base, 'rup_tree.ccp.cut'),
+									"action"			: function (data) {
+										var inst = $.jstree.reference(data.reference),
+											obj = inst.get_node(data.reference);
+										if(inst.is_selected(obj)) {
+											inst.cut(inst.get_top_selected());
+										}
+										else {
+											inst.cut(obj);
+										}
+									}
+								},
+								"copy" : {
+									"separator_before"	: false,
+									"icon"				: false,
+									"separator_after"	: false,
+									"label"				: $.rup.i18nParse($.rup.i18n.base, 'rup_tree.ccp.copy'),
+									"action"			: function (data) {
+										var inst = $.jstree.reference(data.reference),
+											obj = inst.get_node(data.reference);
+										if(inst.is_selected(obj)) {
+											inst.copy(inst.get_top_selected());
+										}
+										else {
+											inst.copy(obj);
+										}
+									}
+								},
+								"paste" : {
+									"separator_before"	: false,
+									"icon"				: false,
+									"_disabled"			: function (data) {
+										return !$.jstree.reference(data.reference).can_paste();
+									},
+									"separator_after"	: false,
+									"label"				: $.rup.i18nParse($.rup.i18n.base, 'rup_tree.ccp.paste'),
+									"action"			: function (data) {
+										var inst = $.jstree.reference(data.reference),
+											obj = inst.get_node(data.reference);
+										inst.paste(obj);
+									}
+								}
+							}
+						}
+					}
 				}
 			},
 			'dnd': {
@@ -1975,22 +2074,4 @@
 				'trim_whitespace': false
 			}
 	};
-
-	//*************************************************
-	// AJUSTES GENERALES DEL FUNCIONAMIENTO DEL PÁTRON
-	//*************************************************
-
-	//Gestion de los literales del menu contextual
-	// TODO
-	/*global.initRupI18nPromise.then(() => {
-    	$.extend(jQuery.jstree.defaults.contextmenu.items().create, {label: $.rup.i18nParse($.rup.i18n.base, 'rup_tree.create')});
-    	$.extend(jQuery.jstree.defaults.contextmenu.items().rename, {label: $.rup.i18nParse($.rup.i18n.base, 'rup_tree.rename')});
-    	$.extend(jQuery.jstree.defaults.contextmenu.items().remove, {label: $.rup.i18nParse($.rup.i18n.base, 'rup_tree.remove')});
-    	$.extend(jQuery.jstree.defaults.contextmenu.items().ccp, {label: $.rup.i18nParse($.rup.i18n.base, 'rup_tree.ccp.label')});
-    	$.extend(jQuery.jstree.defaults.contextmenu.items().ccp.submenu.cut, {label: $.rup.i18nParse($.rup.i18n.base, 'rup_tree.ccp.cut')});
-    	$.extend(jQuery.jstree.defaults.contextmenu.items().ccp.submenu.copy, {label: $.rup.i18nParse($.rup.i18n.base, 'rup_tree.ccp.copy')});
-    	$.extend(jQuery.jstree.defaults.contextmenu.items().ccp.submenu.paste, {label: $.rup.i18nParse($.rup.i18n.base, 'rup_tree.ccp.paste')});
-    }).catch((error) => {
-        console.error('Error en la gestión de los literales del menú contextual:\n', error);
-    });*/
 }));
