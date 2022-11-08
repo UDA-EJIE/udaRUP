30afb02840a6ac8eb419b9192b19ba7212cc8128
Xabi <xaabi6@users.noreply.github.com>
2022-11-08 10:08:57 +0100

    Sustituir comillas dobles por simples
    Como en el resto de la configuración se han usado comillas simples, se sustituyen las dobles por las simples para que el componente sea lo más uniforme posible.

--- src/rup.tree.js
+++ src/rup.tree.js
@@ -1932,15 +1932,15 @@
 				'show_at_node': true,
 				'items': function (o, cb) { // Could be an object directly
 					return {
-						"create" : {
-							"separator_before"	: false,
-							"separator_after"	: true,
-							"_disabled"			: false, //(this.check("create_node", data.reference, {}, "last")),
-							"label"				: $.rup.i18nParse($.rup.i18n.base, 'rup_tree.create'),
-							"action"			: function (data) {
+						'create' : {
+							'separator_before'	: false,
+							'separator_after'	: true,
+							'_disabled'			: false, //(this.check('create_node', data.reference, {}, 'last')),
+							'label'				: $.rup.i18nParse($.rup.i18n.base, 'rup_tree.create'),
+							'action'			: function (data) {
 								var inst = $.jstree.reference(data.reference),
 									obj = inst.get_node(data.reference);
-								inst.create_node(obj, {}, "last", function (new_node) {
+								inst.create_node(obj, {}, 'last', function (new_node) {
 									try {
 										inst.edit(new_node);
 									} catch (ex) {
@@ -1949,29 +1949,29 @@
 								});
 							}
 						},
-						"rename" : {
-							"separator_before"	: false,
-							"separator_after"	: false,
-							"_disabled"			: false, //(this.check("rename_node", data.reference, this.get_parent(data.reference), "")),
-							"label"				: $.rup.i18nParse($.rup.i18n.base, 'rup_tree.rename'),
+						'rename' : {
+							'separator_before'	: false,
+							'separator_after'	: false,
+							'_disabled'			: false, //(this.check('rename_node', data.reference, this.get_parent(data.reference), '')),
+							'label'				: $.rup.i18nParse($.rup.i18n.base, 'rup_tree.rename'),
 							/*!
-							"shortcut"			: 113,
-							"shortcut_label"	: 'F2',
-							"icon"				: "glyphicon glyphicon-leaf",
+							'shortcut'			: 113,
+							'shortcut_label'	: 'F2',
+							'icon'				: 'glyphicon glyphicon-leaf',
 							*/
-							"action"			: function (data) {
+							'action'			: function (data) {
 								var inst = $.jstree.reference(data.reference),
 									obj = inst.get_node(data.reference);
 								inst.edit(obj);
 							}
 						},
-						"remove" : {
-							"separator_before"	: false,
-							"icon"				: false,
-							"separator_after"	: false,
-							"_disabled"			: false, //(this.check("delete_node", data.reference, this.get_parent(data.reference), "")),
-							"label"				: $.rup.i18nParse($.rup.i18n.base, 'rup_tree.remove'),
-							"action"			: function (data) {
+						'remove' : {
+							'separator_before'	: false,
+							'icon'				: false,
+							'separator_after'	: false,
+							'_disabled'			: false, //(this.check('delete_node', data.reference, this.get_parent(data.reference), '')),
+							'label'				: $.rup.i18nParse($.rup.i18n.base, 'rup_tree.remove'),
+							'action'			: function (data) {
 								var inst = $.jstree.reference(data.reference),
 									obj = inst.get_node(data.reference);
 								if(inst.is_selected(obj)) {
@@ -1982,18 +1982,18 @@
 								}
 							}
 						},
-						"ccp" : {
-							"separator_before"	: true,
-							"icon"				: false,
-							"separator_after"	: false,
-							"label"				: $.rup.i18nParse($.rup.i18n.base, 'rup_tree.ccp.label'),
-							"action"			: false,
-							"submenu" : {
-								"cut" : {
-									"separator_before"	: false,
-									"separator_after"	: false,
-									"label"				: $.rup.i18nParse($.rup.i18n.base, 'rup_tree.ccp.cut'),
-									"action"			: function (data) {
+						'ccp' : {
+							'separator_before'	: true,
+							'icon'				: false,
+							'separator_after'	: false,
+							'label'				: $.rup.i18nParse($.rup.i18n.base, 'rup_tree.ccp.label'),
+							'action'			: false,
+							'submenu' : {
+								'cut' : {
+									'separator_before'	: false,
+									'separator_after'	: false,
+									'label'				: $.rup.i18nParse($.rup.i18n.base, 'rup_tree.ccp.cut'),
+									'action'			: function (data) {
 										var inst = $.jstree.reference(data.reference),
 											obj = inst.get_node(data.reference);
 										if(inst.is_selected(obj)) {
@@ -2004,12 +2004,12 @@
 										}
 									}
 								},
-								"copy" : {
-									"separator_before"	: false,
-									"icon"				: false,
-									"separator_after"	: false,
-									"label"				: $.rup.i18nParse($.rup.i18n.base, 'rup_tree.ccp.copy'),
-									"action"			: function (data) {
+								'copy' : {
+									'separator_before'	: false,
+									'icon'				: false,
+									'separator_after'	: false,
+									'label'				: $.rup.i18nParse($.rup.i18n.base, 'rup_tree.ccp.copy'),
+									'action'			: function (data) {
 										var inst = $.jstree.reference(data.reference),
 											obj = inst.get_node(data.reference);
 										if(inst.is_selected(obj)) {
@@ -2020,15 +2020,15 @@
 										}
 									}
 								},
-								"paste" : {
-									"separator_before"	: false,
-									"icon"				: false,
-									"_disabled"			: function (data) {
+								'paste' : {
+									'separator_before'	: false,
+									'icon'				: false,
+									'_disabled'			: function (data) {
 										return !$.jstree.reference(data.reference).can_paste();
 									},
-									"separator_after"	: false,
-									"label"				: $.rup.i18nParse($.rup.i18n.base, 'rup_tree.ccp.paste'),
-									"action"			: function (data) {
+									'separator_after'	: false,
+									'label'				: $.rup.i18nParse($.rup.i18n.base, 'rup_tree.ccp.paste'),
+									'action'			: function (data) {
 										var inst = $.jstree.reference(data.reference),
 											obj = inst.get_node(data.reference);
 										inst.paste(obj);
