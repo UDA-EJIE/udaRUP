fc710b3fc7902ded2483b683acaf0b09fff12926
Adiaz-teknei <adiaz@teknei.com>
2023-06-13 14:04:41 +0200

    Arreglar problema visual en los criterios de filtrado de una tabla
    Se comprueba si hay un checkbox o un radio y modifica el contenido del string del filtro

--- src/rup_table/rup.table.js
+++ src/rup_table/rup.table.js
@@ -1427,6 +1427,11 @@
                     if (fieldName === '' && fieldValue.trim() === '') {
                         continue;
                     }
+                    //Miramos si el elemento es es un checkbox o un radio, en caso de serlo revisamos fieldName para quitar los inputs
+		            if($(field)[0].type === 'checkbox' || $(field)[0].type === 'radio'){
+						let fValue=fieldValue.split('<span>')[1];
+						fieldValue='<span> '+fValue;
+	          		} 
                     searchString = searchString + fieldName + fieldValue + '; ';
                 }
             }
