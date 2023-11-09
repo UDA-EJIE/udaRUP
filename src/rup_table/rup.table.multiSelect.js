19efadbb0173223c3c086ea0691e38ba0629d670
marcos9818 <131156200+marcos9818@users.noreply.github.com>
2023-11-09 08:47:09 +0100

    4 casos de uso diferentes con click derecho en tabla

--- src/rup_table/rup.table.multiSelect.js
+++ src/rup_table/rup.table.multiSelect.js
@@ -380,8 +380,49 @@ handler that will select the items using the API methods.
     function enableMouseSelection(dt) {
         var ctx = dt.settings()[0];
         var container = $(ctx.nTBody);
+        
+        if(ctx.oInit.selectFilaDer){
+	 
+	 container
+            .on('click contextmenu', function (event) {
+                if (event.target !== undefined && event.target.className.indexOf('openResponsive') > -1) {
+                    return false;
+                }
 
-        container
+                var ctx = dt.settings()[0];
+                var row = $(event.target);
+
+                row.triggerHandler('tableSelectBeforeSelectRow',ctx);
+
+                if ((event.shiftKey || event.ctrlKey || event.shiftKey && event.which === 32) && ctx.multiselection.selectedRowsPerPage.length > 0) {
+                    rangeSelection(dt, event);
+                } else if (ctx.multiselection.lastSelectedIsRange) {
+                    if (event.target.type !== 'checkbox') {
+                        deselectAllPage(dt);
+                        dt.row($(event.target).closest('tr').index()).multiSelect();
+                    } else {
+                        $(event.target).parent('').find('input').prop('checked', function () {
+                            return !this.checked;
+                        });
+                    }
+                } else {
+                    if (event.target.type !== 'checkbox') {
+                        rowSelection(dt, event);
+                    } else if(!$(event.target).hasClass('editable')){
+                        $(event.target).parent('').find('input').prop('checked', function () {
+                            return !this.checked;
+                        });
+                    }
+                }
+
+                row.triggerHandler('tableSelectAfterSelectRow',ctx);
+            });
+	
+	
+		} else {
+	
+	
+	container
             .on('click.dtSelect', function (event) {
                 if (event.target !== undefined && event.target.className.indexOf('openResponsive') > -1) {
                     return false;
@@ -415,6 +456,9 @@ handler that will select the items using the API methods.
 
                 row.triggerHandler('tableSelectAfterSelectRow',ctx);
             });
+}
+
+       
     }
 
     /**
@@ -1615,7 +1659,7 @@ handler that will select the items using the API methods.
 
             if (style !== 'api') {
                 if (ctx.oInit.multiSelect.enableMouseSelection) {
-                    enableMouseSelection(dt);
+                    enableMouseSelection(dt, ctx);
                 }
 
                 if (ctx.oInit.multiSelect.enableKeyboardSelection) {
