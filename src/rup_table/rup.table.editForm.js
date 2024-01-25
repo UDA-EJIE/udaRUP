b7e8201ab9fab24dff290a95edec75f44828fa47
Xabi <xaabi6@users.noreply.github.com>
2024-01-25 14:11:29 +0100

    Quitar llamadas a métodos eliminados

--- src/rup_table/rup.table.editForm.js
+++ src/rup_table/rup.table.editForm.js
@@ -1126,11 +1126,6 @@
             } else if (isDeleting || ctx.oInit.formEdit.idForm.valid()) {
 				// Obtener el valor del parámetro HDIV_STATE (en caso de no estar disponible se devolverá vacío) siempre y cuando no se trate de un deleteAll porque en ese caso ya lo contiene el filtro
 				if (url.indexOf('deleteAll') === -1) {
-					// Elimina los campos _label generados por los autocompletes que no forman parte de la entidad
-					$.fn.deleteAutocompleteLabelFromObject(ajaxOptions.data);
-
-					// Elimina los campos autogenerados por los multicombos que no forman parte de la entidad
-					$.fn.deleteMulticomboLabelFromObject(ajaxOptions.data, ctx.oInit.formEdit.detailForm);
 
 					var hdivStateParamValue = $.fn.getHDIV_STATE(undefined, ctx.oInit.formEdit.idForm);
 					if (hdivStateParamValue !== '') {
