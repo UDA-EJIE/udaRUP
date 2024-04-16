e458eaf65f97e2ecb3330f270fa7b05232fa95b7
Xabi <xaabi6@users.noreply.github.com>
2024-04-16 10:37:04 +0200

    Corregir generación de URL en rup_select enlazados
    (cherry picked from commit f86d9f6e24fd5a2a9575c5dbaf4b27e6348cef61)

--- src/rup.select.js
+++ src/rup.select.js
@@ -1207,7 +1207,10 @@
 					        			}
 			        				}
 			        			});
-			        		}
+							} else if (params.url.indexOf(datosParent) < 0) {
+								// Aseguramos que mete el valor del padre.
+								params.url = params.url + '?' + datosParent;
+							}
 			        		$request = $.ajax(params);
 			        	}
 			        }else{
