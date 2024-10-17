b6ccec7af68baf0884027a8c3ad2aa11533f9c0c
Hugo Martinez Garcia <hmartinez@teknei.com>
2024-10-17 10:11:41 +0200

    Mejoras rup select
    Propiedad spaceEnable y en autocompelte deseleciona bien en todas los casos
    
    (cherry picked from commit 4c898ad7fd7af8db4023fbbebf27c835ce26a4aa)

--- src/rup.select.js
+++ src/rup.select.js
@@ -754,9 +754,9 @@
          * @function option
          * @example
          * // Establecer una propiedad
-         * $("#idSelect").rup_select("option", "minLegth", 2);
+         * $("#idSelect").rup_select("option", "minimumResultsForSearch", 2);
          * // Establecer varias propiedad
-         * $("#idSelect").rup_select("option", {minLegth:2, delay:1000});
+         * $("#idSelect").rup_select("option", {minimumResultsForSearch:2, delay:1000});
          */
 		option: function (optionName, value, removeOptions) {
         	let settings = $(this).data('settings');
@@ -1304,7 +1304,14 @@
 									}
 								});
 							} else {
-								$('#' + settings.id).rup_select('setRupValue', seleccionado.length == 1 ? seleccionado[0].id : settings.blank);
+								if(settings.autocomplete){
+								  let valorInput = mySelect.selection.$selection.find('input').val() 
+								  $('#' + settings.id).rup_select('setRupValue', seleccionado.length == 1 ? seleccionado[0].id : settings.blank);
+								  mySelect.selection.$selection.find('input').val(valorInput); 
+								  mySelect.selection.$selection.find('input').focus();
+								}else{
+									$('#' + settings.id).rup_select('setRupValue', seleccionado.length == 1 ? seleccionado[0].id : settings.blank);
+								}
 							}
 				          
 				         if (settings.onLoadSuccess !== null && settings.onLoadSuccess !== undefined) {
@@ -1388,6 +1395,10 @@
                  }
         		if(settings.autocomplete){
         			$('#' + settings.id).select2MultiCheckboxes(settings);
+					if(settings.spaceEnable){//permitir en la busqueda en espacio
+						sel = $('#' + settings.id).data('select2').selection;
+						sel.$selection.off('keydown');
+					}
         		}else{
         			$('#' + settings.id).select2(settings);
         		}
@@ -1810,8 +1821,11 @@
 	                			}
 	        
 	                			$('#' + settings.id).select2MultiCheckboxes(settings);
+								let mySelect2 = $('#' + settings.id).data('select2');
+								if(settings.spaceEnable){//permitir en la busqueda en espacio
+									mySelect2.$selection.off('keydown');
+								}
 	                			if(settings.defaultValue != undefined){
-	                				let mySelect2 = $('#' + settings.id).data('select2');
 	                				mySelect2.$selection.find('input').val(settings.defaultValue);
 	                				if(settings.selected == undefined && mySelect2.dataAdapter._dataToConvert != undefined && mySelect2.dataAdapter._dataToConvert.length > 0){
 		                			    let data = $.grep(mySelect2.dataAdapter._dataToConvert, function (v) {
@@ -1832,6 +1846,7 @@
 			                	mySelect2.on('close', function (e) {
 				                	if (Object.keys(e).length === 1) {
 				                	  mySelect2.$selection.find('input').val('');
+									  settings.selected = undefined;
 					                  $('#' + settings.id).val(null).trigger('change');
 					                  if(!settings.closeOnSelect){
 					                	  $('#' + settings.id).select2('open');
@@ -1855,7 +1870,8 @@
                 			let remotoSelect = $('#' + settings.id).data('select2');
                 			remotoSelect.on('close', function (e) {
 			                	if (Object.keys(e).length === 1) {
-			                		remotoSelect.$selection.find('input').val('');
+			                	  remotoSelect.$selection.find('input').val('');
+								  settings.selected = undefined;	
 				                  $('#' + settings.id).val(null).trigger('change');
 				                  if(!settings.closeOnSelect){
 				                	  $('#' + settings.id).select2('open');
@@ -2084,19 +2100,22 @@
 	 * @property {boolean} [autocomplete=false] - Habilita la funcionalidad de
 	 *           autocompletado, permitiendo hacer búsquedas sobre los resultados.
 	 */
-	$.fn.rup_select.defaults = {
-		onLoadError: null,
-		width: '100%',
-		customClasses: ['select-material'],
-		blank: "-1",
-		minimumResultsForSearch: Infinity,
-		submitAsJSON: false,
-		dataType: 'json',
-		cache: true,
-		multiple: false,
-		defaultValueAutocompleteNotLoaded: false,
-		multiValueToken: '##'
-	};
+    $.fn.rup_select.defaults = {
+        onLoadError: null,
+        width: '100%',
+        customClasses: ['select-material'],
+        blank: "-1",
+        minimumResultsForSearch: Infinity,
+        submitAsJSON: false,
+        dataType: 'json',
+        cache: true,
+        multiple: false,
+        defaultValueAutocompleteNotLoaded: false,
+        multiValueToken:'##',
+		spaceEnable:true
+        };
+
+
 }));
 
 function chargedStyles(data){
