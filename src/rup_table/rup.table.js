6f0631095b34e4a4a506c9ccacd64fd949a996e1
hmartinez85 <34453480+hmartinez85@users.noreply.github.com>
2025-02-27 16:27:15 +0100

    el select siendo pk no lo hacía bien al editar
    (cherry picked from commit 7ddcc5ebf0856fc772aa0fe3318420f844cee65e)

--- src/rup_table/rup.table.js
+++ src/rup_table/rup.table.js
@@ -469,8 +469,10 @@
                             // Comprobamos si es un componente rup o no. En caso de serlo usamos el metodo disable.
                             if (input.attr('ruptype') === 'date' && !input.rup_date('isDisabled')) {
                                 input.rup_date('disable');
-                            } else if (input.attr('ruptype') === 'combo' && !input.rup_combo('isDisabled')) {
+							} else if (input.attr('ruptype') === 'combo' && !input.rup_combo('isDisabled')) {
                                 input.rup_combo('disable');
+							} else if (input.attr('ruptype') === 'select' && !input.rup_select('isDisabled')) {
+                                input.rup_select('disable');
                             } else if (input.attr('ruptype') === 'time' && !input.rup_time('isDisabled')) {
                                 input.rup_time('disable');
                             } else if (input.attr('type') === 'checkbox') {
