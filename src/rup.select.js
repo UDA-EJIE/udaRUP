e44046542c31a4539f28df52b24024398bde693c
Xabi <xaabi6@users.noreply.github.com>
2025-03-06 15:53:43 +0100

    Escapar caracteres especiales en dos selectores
    Este commit aplica los cambios realizados en 31ac9deeeac880f20f2176f0c0f3b1ea5773909a sobre los cambios de 0f04b97b3f173b800fbe47ec926e8f465030d166.

--- src/rup.select.js
+++ src/rup.select.js
@@ -1369,7 +1369,7 @@
 							if(settings.cache){
 								settings.selected = "";
 							}else{
-								settings.selected = $('#' + settings.id).rup_select('getRupValue');
+								settings.selected = $('#' + $.escapeSelector(settings.id)).rup_select('getRupValue');
 							}
 	              		  }
 				        });
