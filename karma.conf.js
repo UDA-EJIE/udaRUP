d138c05909add6408c8e76c5af4c167ab86f98e3
Xabi <xaabi6@users.noreply.github.com>
2025-03-21 15:48:50 +0100

    Actualizar versión en reporte de Karma

--- karma.conf.js
+++ karma.conf.js
@@ -45,9 +45,9 @@ module.exports = function (config) {
             templatePath: null, // set if you moved jasmine_template.html
             focusOnFailures: false, // reports show failures on start
             namedFiles: true, // name files instead of creating sub-directories
-            pageTitle: 'Karma Report 6.0.0', // page title for reports; browser info by default
+            pageTitle: 'Karma Report 6.2.0', // page title for reports; browser info by default
             urlFriendlyName: false, // simply replaces spaces with _ for files/dirs
-            reportName: 'karma_report_6.0.0', // report summary filename; browser info by default
+            reportName: 'karma_report_6.2.0', // report summary filename; browser info by default
 
             // experimental
             preserveDescribeNesting: false, // folded suites stay folded 
