6e9b1edfb4530f0fcfc82f341cd6616bb3bad209
Xabi <xaabi6@users.noreply.github.com>
2025-04-10 08:57:48 +0200

    Eliminar iteración innecesaria
    (cherry picked from commit 82c30d383576b273a0510e24b7323df94fc647ff)

--- src/rup_table/rup.table.js
+++ src/rup_table/rup.table.js
@@ -670,9 +670,6 @@
             		})
             	}
             });
-            
-            $.each(options.colModel, function (index, column) {
-            });
 
             //se crea el tfoot
             var $tfoot = $('<tfoot>').appendTo($self[0]);
