45ee40d099b055155dff4198077f3aaa5fa12a88
hmartinez85 <34453480+hmartinez85@users.noreply.github.com>
2025-09-30 10:48:55 +0200

    se añade todo el módulo jquery-ui por si se quiere usar

--- entry.js
+++ entry.js
@@ -2,13 +2,7 @@ import $ from 'jquery';
 window.jQuery = $;
 window.$ = $;
 
-import 'jquery-ui/ui/effects/effect-slide';
-import 'jquery-ui/ui/effect'; // Core de efectos
-
-// Cargar dependencias con orden explícito
-import 'jquery-ui/ui/widgets/dialog';
-import 'jquery-ui/ui/widgets/datepicker'; // <- requerido primero
-import 'jquery-ui/ui/widgets/tabs';
+import 'jquery-ui-dist/jquery-ui.js';
 import './src/index.js'; 
 
 
